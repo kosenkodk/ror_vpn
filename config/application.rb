90102317c9d41649b5e71a5db2ed6acaa51b8c3e
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-09-01 13:43:49 +0300

    Use production mode

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module VegaVPN
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.hosts << "vega.isit.su"
+    config.hosts << 'vega.isit.su'
+    config.hosts << 'localhost'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
