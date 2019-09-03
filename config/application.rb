e106746e4579ebe853935d99e4e13c5ef1989430
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-09-03 12:38:26 +0300

    Fonts error - no route matches

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,6 @@ module VegaVPN
     config.exceptions_app = self.routes
     config.hosts << 'vega.isit.su'
     config.hosts << 'localhost'
-    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
