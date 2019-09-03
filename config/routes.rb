ea2f3c10b0e10b81c2e07b6a85a0651e50546365
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-09-03 14:20:39 +0300

    test 404 page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
-  get '/not_found', to: "errors#not_found"
-  get '/404', to: "errors#not_found"
+  get '/404', to: 'errors#not_found'
+  # match '/404' => 'errors#not_found', via: :get, as: :not_found
   get '/422', to: 'errors#unacceptable'
   get '/200', to: 'errors#coming_soon'
   get '/500', to: 'errors#internal_error'
