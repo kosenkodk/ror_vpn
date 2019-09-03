f653da4ed2163eb01fb8b0117ff6472253d148f3
Dmitry K <kosenkodk@gmail.com>
2019-09-03 15:07:09 +0300

    Test error page and back to home page button

---
+++ spec/features/error_page_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe ErrorsController, type: :feature do 
+  describe 'Get 404 Page' do
+    it 'renders not_found template' do
+      visit('/404')
+      expect(page).to have_content('404')
+      expect(page).to have_content('Oh no!!!')
+      expect(page).to have_content('You’re either misspelling the URL or requesting a page that’s no longer here.')
+    end
+    it 'redirect to home page after click on "Back to previous page" button' do
+      visit('/404')
+      click_on('Back to previous page')
+      expect(page).not_to have_content('404')
+    end
+  end
+end
