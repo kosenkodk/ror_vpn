3877c2117701aee5df580d58cd5b0a39b9cf0392
Dmitry K <kosenkodk@gmail.com>
2019-09-03 14:20:39 +0300

    test 404 page

--- spec/controllers/errors_controller_spec.rb
+++ spec/controllers/errors_controller_spec.rb
@@ -1,11 +1,9 @@
 require 'rails_helper'
-# require 'errors_controller'
 
 RSpec.describe ErrorsController do #, type: :controller do
   describe "GET " do
     it "renders :404 template" do
-      # get not_found_path
-      get :not_found
+      get "/404"
       expect(response).to render_template(:not_found)
     end
   end
