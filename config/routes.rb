9c6ad9544c77ec71dab5eb9453ef1a95993fb9fc
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-09-04 11:25:59 +0300

    Footer Section. Added link to the ‘coming soon’ page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
-  get '/404', to: 'errors#not_found'
+  get '/404', to: 'errors#not_found', as: :not_found
   get '/422', to: 'errors#unacceptable'
-  get '/200', to: 'errors#coming_soon'
+  get '/200', to: 'errors#coming_soon', as: :coming_soon
   get '/500', to: 'errors#internal_error'
   resource :contacts
   resource :contact
