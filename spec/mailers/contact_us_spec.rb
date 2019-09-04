4f6bd80ecd1243d213082fe4342bd600593e4555
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-09-04 23:12:47 +0300

    Contact us. Notify admin too.

--- spec/mailers/contact_us_spec.rb
+++ spec/mailers/contact_us_spec.rb
@@ -1,20 +1,41 @@
 require "rails_helper"
 
 RSpec.describe ContactUsMailer, type: :mailer do
-  include Rails.application.routes.url_helpers # it seems optional
+  include Rails.application.routes.url_helpers
 
   let(:contact_id) { 1 }
-  let(:email) { ContactUsMailer.notify_user('author@email.ru', contact_id).deliver_now }
 
-  it 'has correct email' do
-    expect(email.to).to include('author@email.ru')
+  context 'notify user' do
+    let(:email) { ContactUsMailer.notify_user('user@email.ru', contact_id).deliver_now }
+    
+    it 'with correct email' do
+      expect(email.to).to include('user@email.ru')
+    end
+
+    it 'with correct subject' do
+      expect(email.subject).to eq('Congrats with your new message!')
+    end
+
+    it 'with contact link in body message' do
+      expect(email.body.to_s).to include(contact_url(contact_id))
+    end
+
+    it 'with not empty email template'
   end
 
-  it 'has correct subject' do
-    expect(email.subject).to eq('Congrats with your new message!')
-  end
+  context 'notify admin' do
+    let(:admin_email) { ContactUsMailer.notify_admin(Rails.application.credentials.admin_email, contact_id).deliver_now }
+    
+    it 'with correct email' do
+      expect(admin_email.to).to include('admin@email.ru')
+    end
 
-  it 'has contact link in body message' do
-    expect(email.body.to_s).to include(contact_url(contact_id))
+    it 'with correct subject' do
+      expect(admin_email.subject).to eq('Contact Us. New message.')
+    end
+
+    it 'with contact link in body message' do
+      expect(admin_email.body.to_s).to include(contact_url(contact_id))
+    end
   end
 end
