8315f89d10e2983ed0cfa434fa3bf33d8c37f1b9
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-09-04 11:19:10 +0300

    test contact us link in footer section of the  home page

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -24,5 +24,12 @@ RSpec.describe HomeController, type: :feature do
       expect(page).to have_content('SUPPORT')
       expect(page).to have_content('CONTACT')
     end
+    it 'renders contact us page' do
+      visit root_path
+      click_on('Contact Us')
+      expect(page).to have_content('Contact us')
+      expect(page).to have_content('Send us a message and we will be in touch within 24 hours.')
+      expect(page).to have_content('Submit')
+    end
   end
 end
