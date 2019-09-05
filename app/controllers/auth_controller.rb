b56077cd043cd1856bb410a9b2f0ba613413d7cd
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-09-05 19:27:25 +0300

    Sign up with email, password and confirm password.

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,22 +1,29 @@
 class AuthController < ApplicationController
   layout 'no_header_and_footer'
-
+  before_action :user_new, except: [:success]
+  
   def login
-    @user = User.new
     render layout: 'no_footer'
   end
 
+  def signup
+    render layout: 'application'
+  end
+
   def forgot_pwd
-    @user = User.new
   end
 
   def reset_pwd
     # if User.exists?(code: params[:code])
     # @user = User.find_by(code: params[:code])
-    @user = User.new
   end
 
   def success
     
+  end
+  
+  private
+  def user_new
+    @user = User.new
   end
 end
