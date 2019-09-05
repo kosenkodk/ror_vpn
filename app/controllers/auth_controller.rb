011a1d28c94fc25b2b0b20ce85c1aefce496890a
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-09-05 14:14:09 +0300

    Forgot Password Page with html markup (render template only)

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,7 +1,13 @@
 class AuthController < ApplicationController
-  layout 'application_without_footer'
+  # layout 'application_without_footer'
+  
   def login
     @user = User.new
-    # render layout: 'application_without_footer'
+    render layout: 'no_footer'
+  end
+
+  def forgot
+    @user = User.new
+    render layout: 'no_header_and_footer'
   end
 end
--- spec/features/auth_page_spec.rb
+++ spec/features/auth_page_spec.rb
@@ -15,4 +15,18 @@ RSpec.describe AuthController, type: :feature do
       expect(page).not_to have_css('footer')
     end
   end
+
+  describe 'GET :forgot' do
+    it 'render :forgot template' do
+      visit(forgot_path)
+      expect(find('.forgot_pwd')).to have_content(I18n.t('pages.forgot_pwd.title'))
+      expect(find('.forgot_pwd')).to have_content(I18n.t('pages.forgot_pwd.subtitle'))
+      expect(find('label').text).to eq('Email address')
+      page.has_selector?('input.user_email')
+      expect(find_button('Submit').value).to eq('Submit')
+      expect(page).not_to have_css('footer')
+      expect(page).not_to have_css('header')
+    end
+  end
+
 end
