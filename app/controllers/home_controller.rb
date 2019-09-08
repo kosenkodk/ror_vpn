051614009d71d282d194a5b7719f5bc83b9ae091
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-09-08 12:42:03 +0300

    pricing page (view, route, new layout, nav menu link)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
+
   def index
     @plan_active_index = 0
     @payment_method_item_active_index = 1
   end
+
+  def pricing
+    render layout: 'bg_fit_container'
+  end
+  
 end
---
+++ spec/features/pricing_page_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe HomeController, type: :feature do
+  describe 'Pricing Page' do
+    it 'display :new form' do
+      visit pricing_path
+      
+      expect(page).to have_content('One price for all your devices')
+      expect(page).to have_content('Start my free trial')
+      expect(page).to have_content('Why the Media loves VPN')
+      
+      # expect(page).to have_content('Questions & Answers')
+
+      expect(page).to have_css('nav')
+      expect(page).to have_css('footer')
+    end
+  end
+end
