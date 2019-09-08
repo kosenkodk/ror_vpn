49a7b43ca802f4cc0cf56e159717f1f601986999
Dmitry K <kosenkodk@gmail.com>
2019-09-08 10:54:58 +0300

    helper method for sign up active item

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,7 @@
 module HomeHelper
+  def signup_icon_active item, item_active
+    image_name = item == item_active ? 'signup/on.png' : 'signup/off.png'
+    active_class = item == item_active ? 'active' : ''
+    return image_name, active_class
+  end
 end
