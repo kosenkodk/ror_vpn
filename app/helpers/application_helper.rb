4f7fe7b13082d815ba4de83ccea7edf8df8067f3
Dmitry K <kosenkodk@gmail.com>
2019-09-08 14:09:06 +0300

    test active items of sign up page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,3 @@
 module ApplicationHelper
-  def signup_icon_active item, item_active
-    image_name = item == item_active ? 'signup/on.png' : 'signup/off.png'
-    active_class = item == item_active ? 'active' : ''
-    return image_name, active_class
-  end
+  
 end
