4dc65e7b75b66fbe12ef26e2888784c0fd6e9483
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-09-09 16:16:07 +0300

    Pricing Page. Tariff Plans.

--- db/schema.rb
+++ db/schema.rb
@@ -10,38 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_09_120357) do
-
-  create_table "action_text_rich_texts", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "body"
-    t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
-  end
-
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
+ActiveRecord::Schema.define(version: 2019_09_09_121116) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "email"
@@ -53,19 +22,13 @@ ActiveRecord::Schema.define(version: 2019_09_09_120357) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "plans", force: :cascade do |t|
-    t.string "title"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "tariff_plans", force: :cascade do |t|
     t.string "title"
     t.float "price"
     t.integer "duration"
     t.float "price_duration"
     t.float "price_duration_sale"
-    t.string "price_duration_sale_comment"
+    t.string "price_comment"
     t.text "features"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -79,5 +42,4 @@ ActiveRecord::Schema.define(version: 2019_09_09_120357) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
---
+++ spec/views/tariff_plans/edit.html.erb_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe "tariff_plans/edit", type: :view do
+  before(:each) do
+    @tariff_plan = assign(:tariff_plan, TariffPlan.create!(
+      :title => "MyString",
+      :price => 1.5,
+      :duration => 1,
+      :price_duration => 1.5,
+      :price_duration_sale => 1.5,
+      :price_comment => "MyString",
+      :features => "MyText"
+    ))
+  end
+
+  it "renders the edit tariff_plan form" do
+    render
+
+    assert_select "form[action=?][method=?]", tariff_plan_path(@tariff_plan), "post" do
+
+      assert_select "input[name=?]", "tariff_plan[title]"
+
+      assert_select "input[name=?]", "tariff_plan[price]"
+
+      assert_select "input[name=?]", "tariff_plan[duration]"
+
+      assert_select "input[name=?]", "tariff_plan[price_duration]"
+
+      assert_select "input[name=?]", "tariff_plan[price_duration_sale]"
+
+      assert_select "input[name=?]", "tariff_plan[price_comment]"
+
+      assert_select "textarea[name=?]", "tariff_plan[features]"
+    end
+  end
+end
