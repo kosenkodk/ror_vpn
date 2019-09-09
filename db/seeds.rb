7c6c6643341c8701a8594f80a48bd70f7266bc9c
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-09-09 18:58:16 +0300

    Sign up page > plan item (improved html markup with css styles)

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,9 @@ features = 'All Countries 5,Devices simultaneously,High Speed,Unlimited traffic'
 features_free = '1 Country,1 Device,Simultaneously,Medium speed,No torrenting,IKEv2 or OpenVPN'
 [
   { title: 'Plan for 1 year', price: 2.99, duration: 0, price_duration: 430.20, price_duration_sale: 322.65, price_comment: '$ 107.55 every 3 years', features: features },
-  { title: 'Quarterly plan ', price: 3.99, duration: 0, price_duration: 286.80, price_duration_sale: 191.05, price_comment: '$ 95.75 every 2 years', features: features },
+  { title: 'Quarterly plan', price: 3.99, duration: 0, price_duration: 286.80, price_duration_sale: 191.05, price_comment: '$ 95.75 every 2 years', features: features },
   { title: 'Plan for 1 month', price: 6.99, duration: 0, price_duration: 143.40, price_duration_sale: 59.52, price_comment: '$ 83.88 every year', features: features },
-  { title: 'Free ', price: 0.00, duration: 0, price_duration: 0.00, price_duration_sale: 0.00, price_comment: '', features: features_free }
+  { title: 'Free plan', price: 0.00, duration: 0, price_duration: 0.00, price_duration_sale: 0.00, price_comment: '', features: features_free }
 ].each do |item|
 TariffPlan.find_or_create_by(
   title: item[:title], price: item[:price], 
