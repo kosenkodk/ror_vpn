46953e40afec0b14b946d863e86328ddb4edaf23
Dmitry K <kosenkodk@gmail.com>
2019-09-09 17:24:22 +0300

    Pricing page with dynamic content

--- spec/features/pricing_page_spec.rb
+++ spec/features/pricing_page_spec.rb
@@ -6,14 +6,19 @@ RSpec.describe HomeController, type: :feature do
       visit pricing_path
       
       expect(page).to have_content('One price for all your devices')
-      expect(page).to have_content('Start my free trial')
+
+      # click_on('#pills-plan3')
+      # click_button 'Free'
+      # expect(find('.card .active')).to have_text('0.0')
+
+      expect(page).to have_content('Start my free trial') # button
 
       # expect(page).to have_css('reviews')
       expect(page).to have_content('Why the Media loves VPN')
       
       expect(page).to have_content('All plans include')
       
-      # TODO: after faq implementation
+      # TODO: uncomment after faq implementation
       # expect(page).to have_content('Questions & Answers')
       
       expect(page).to have_css('nav')
