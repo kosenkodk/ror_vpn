1a59c28f629c5da014d9f26b31d0c03fdc1209ca
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-09-10 12:53:23 +0300

    Payment methods with static content (qiwi and credit card were added)

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,5 +1,13 @@
 module HomeHelper
   
+  def get_active_checkbox_icon
+    'signup/on.png'
+  end
+
+  def get_inactive_checkbox_icon
+    'signup/off.png'
+  end
+
   def get_img_and_class_by_active_item_index item, item_active_index
     image_name = item == item_active_index ? 'signup/on.png' : 'signup/off.png'
     active_class = item == item_active_index ? 'active' : ''
