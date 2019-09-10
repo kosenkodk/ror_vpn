eb99fd722792be8434c8fa945c58d6c08a6c8918
Dmitry K <kosenkodk@gmail.com>
2019-09-10 14:21:21 +0300

    fix features specs

--- spec/features/pricing_page_spec.rb
+++ spec/features/pricing_page_spec.rb
@@ -2,15 +2,23 @@ require 'rails_helper'
 
 RSpec.describe HomeController, type: :feature do
   describe 'Pricing Page' do
+    
+    let!(:plan) { FactoryBot.create :tariff_plan, title: 'Plan for 1 year', price: 1.00 }
+    let!(:plan_free) { FactoryBot.create :tariff_plan, title: 'Free plan', price: 0.00 }
+
     it 'display :new form' do
       visit pricing_path
       
       expect(page).to have_content('One price for all your devices')
 
-      # click_on('#pills-plan3')
-      # click_button 'Free'
-      # expect(find('.card .active')).to have_text('0.0')
+      click_on('Free plan')
+      expect(page).to have_content('Free')
+      # click_on('#pills-plan1')
+      # click_button 'Free plan'
+      # expect(find('#pills-tabPlanContent .card .active')).to have_text('Free')
+      # expect(find('.pricing .card-title.pricing-card-title', match: :first)).to have_text('Free')
 
+      click_on('Plan for 1 year', match: :first)
       expect(page).to have_content('Start my free trial') # button
 
       # expect(page).to have_css('reviews')
