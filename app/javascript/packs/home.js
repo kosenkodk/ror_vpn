821216675c900b6982120cb92bf115f49c663148
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-09-11 21:59:30 +0300

    sign up page > clickable plan and payment method items

---
+++ app/javascript/packs/home.js
@@ -0,0 +1,35 @@
+$(jQuery).on('ready turbolinks:load', function () {
+
+  // $('.plans button.active').show();
+  $('.plans button.active').removeClass('d-none');
+
+  $('.payment_methods .card').on('click hover', function () {
+    $('.payment_methods .card').removeClass('active');
+    $('.payment_methods .card .icon-checkbox').removeClass('active');
+    $(this).addClass('active');
+    $(this).find('.icon-checkbox').addClass('active');
+  }).mouseover(function () {
+
+  }).mouseout(function () {
+
+  })
+
+  $('.plans .card').on('click hover', function () {
+    $('.plans .card').removeClass('active');
+    $('.plans .card .icon-checkbox').removeClass('active');
+
+    // $('.plans button').hide();
+    $('.plans button').addClass('d-none');
+
+    $(this).addClass('active');
+    $(this).find('.icon-checkbox').addClass('active');
+
+    // $(this).find('button').show();
+    $(this).find('button').removeClass('d-none');
+  }).mouseover(function () {
+
+  }).mouseout(function () {
+
+  })
+
+});
