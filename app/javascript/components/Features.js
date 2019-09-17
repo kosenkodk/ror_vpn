de34289caa67db3bfe286d8f81241d8e5401d3c6
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-09-17 18:18:55 +0300

    server rendering

--- app/javascript/components/Features.js
+++ app/javascript/components/Features.js
@@ -1,33 +1,33 @@
 import React from "react"
 import PropTypes from "prop-types"
 class Features extends React.Component {
-render() {
-return (
-<React.Fragment>
-  <div id="features" className="features row">
-    {this.props.features.map(item => (
-    <div key={item.id} className="col-xs-12 col-sm-6 col-md-4">
-      <div className="card">
-        <div className="card-header">
-          {/* <img src="assets/icons/ic_facebook.png" className="card-img-top" alt="..." /> */}
-          <img src={item.icon_url} />
-          <h5 className="card-title">{item.title}</h5>
+  render() {
+    return (
+      <React.Fragment>
+        <div id="features" className="features row">
+          {this.props.features.map(item => (
+            <div key={item.id} className="col-xs-12 col-sm-6 col-md-4">
+              <div className="card">
+                <div className="card-header">
+                  {/* <img src="assets/icons/ic_facebook.png" className="card-img-top" alt="..." /> */}
+                  <img src={item.icon_url} />
+                  <h5 className="card-title">{item.title}</h5>
+                </div>
+                <div className="card-body">
+                  <h5 className="card-title">{item.subtitle}</h5>
+                  <p className="card-text">{item.text}</p>
+                </div>
+              </div>
+            </div>
+          ))
+          }
         </div>
-        <div className="card-body">
-          <h5 className="card-title">{item.subtitle}</h5>
-          <p className="card-text">{item.text}</p>
-        </div>
-      </div>
-    </div>
-    ))
-    }
-  </div>
-</React.Fragment>
-);
-}
+      </React.Fragment>
+    );
+  }
 }
 
 Features.propTypes = {
-features: PropTypes.array
+  features: PropTypes.array
 };
 export default Features
