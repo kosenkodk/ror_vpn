c05b8a1fd0a10cc2a7d18f62114b1d7597972310
Dmitry K <kosenkodk@gmail.com>
2019-09-17 20:16:32 +0300

    API v1. Feature show.

--- app/controllers/api/v1/features_controller.rb
+++ app/controllers/api/v1/features_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::FeaturesController < ApplicationController
     features = Feature.all
     render json: features.as_json(only: [:id, :title, :subtitle, :text], methods: [:icon_url])
   end
+
+  def show
+    @feature = Feature.find(params[:id])
+    render json: @feature.attributes.merge({icon_url: url_for(@feature.icon)})
+  end
 end
