e600e487f971f1df4dbe13e6d9ed7b948ca8486d
Dmitry K <kosenkodk@gmail.com>
2019-09-19 01:41:30 +0300

    Server side rendering for features react component

--- app/javascript/components/Features.js
+++ app/javascript/components/Features.js
@@ -5,30 +5,38 @@ class Features extends React.Component {
 
   constructor(props) {
     super(props);
+    let features = props.features && props.features.length > 0 ? props.features : []
+
     this.state = {
-      features: []
+      features: features
     };
   }
 
   render() {
     const { features } = this.state;
     return (
-      <React.Fragment>
-        <div id="features" className="features row">
-          {
-            features.map(item => (
-              <div key={item.id} className="col-xs-12 col-sm-6 col-md-4">
-                <Feature {...item} />
-              </div>
-            ))
-          }
-        </div>
-      </React.Fragment>
+      <div id="features" className="features row">
+        <h1>features {features.length}</h1>
+        {
+          features.map(item => (
+            <div key={item.id} className="col-xs-12 col-sm-6 col-md-4">
+              <Feature {...item} />
+              {/* <Feature title={item.title} /> */}
+            </div>
+          ))
+        }
+      </div>
     );
   }
 
   componentDidMount() {
-    if (this.state.features.count > 0) return;
+    if (this.props.features && this.props.features.length > 0) {
+      console.log('use props')
+      // this.setState({ features: this.props.features })
+      return;
+    }
+    console.log('getting data from api...')
+
     const url = "api/v1/features";
     fetch(url)
       .then(response => {
@@ -38,13 +46,19 @@ class Features extends React.Component {
         throw new Error("Network response was not ok.");
       })
       .then(response => this.setState({ features: response }))
-      .catch(() => this.props.history);
+      .catch((err) => {
+        console.log(err)
+        this.props.history
+
+      });
+
   }
 
 }
 
 Features.propTypes = {
-  features: PropTypes.array
+  features: PropTypes.node,
+  state: PropTypes.node
 };
 
 export default Features
