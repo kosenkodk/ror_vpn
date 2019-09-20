838fc82c51ff20d4d29a6c4b84794158e80d20e1
Dmitry K <kosenkodk@gmail.com>
2019-09-20 19:41:16 +0300

    App.js with server side rendering and react-router-dom (but login form is not working when js has been disabled )

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,44 +1,30 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+// import PropTypes from 'prop-types'
+import { Route } from 'react-router-dom'
 import Login from './Login'
 import Features from './Features'
-// import Plans from './Plans'
-// import history from 'history'
-import { createBrowserHistory, createMemoryHistory } from 'history';
+import Router from './Router'
 
-class App extends React.Component {
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return (
+//
+//     );
+//   }
+// }
+// App.propTypes = {
+//   token: PropTypes.string,
+// };
 
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    // const history = createMemoryHistory();
-    const isServer = true
-    const history = isServer
-      ? createMemoryHistory({
-        initialEntries: ['/']
-      })
-      : createBrowserHistory();
-    return (
-      // <BrowserRouter>
-      // <Switch>
-      <Router history={history}>
-        <Route path="/" exact render={(props) => (<div>Helloworld</div>)} />
-        {/* <Route exact path="/react_app" render={() => "react app > Home!"} /> */}
-        {/* <Route exact path="/login" render={() => <Login />} /> */}
-        {/* <Route exact path="/features" render={() => <Features />} /> */}
-        {/* <Route exact path="/tariff_plans" render={() => <Plans />} /> */}
-      </Router>
-      // </Switch>
-      // </BrowserRouter>
-    );
-  }
-}
-
-App.propTypes = {
-  token: PropTypes.string,
-};
+const App = props => (
+  <Router path={props.path} context={props}>
+    <Route path="/" exact render={(props) => (<div>token: {props.token}</div>)} />
+    <Route exact path="/login" render={() => <Login />} />
+    <Route exact path="/features" render={() => <Features />} />
+  </Router>
+)
 
 export default App
