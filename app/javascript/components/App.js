1dff8bcf0655ea3e66d1cabd461e9920b126f3c2
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-09-20 16:14:01 +0300

    Encountered error "#<ExecJS::ProgramError: Error: Invariant failed: Browser history needs a DOM>" when prerendering App with {"token":""}

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,23 +1,44 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Login from './Login'
 import Features from './Features'
 // import Plans from './Plans'
+// import history from 'history'
+import { createBrowserHistory, createMemoryHistory } from 'history';
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+    // const history = createMemoryHistory();
+    const isServer = true
+    const history = isServer
+      ? createMemoryHistory({
+        initialEntries: ['/']
+      })
+      : createBrowserHistory();
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/react_app" render={() => "react app > Home!"} />
-          <Route exact path="/login" render={() => <Login />} />
-          <Route exact path="/features" render={() => <Features />} />
-          {/* <Route exact path="/tariff_plans" render={() => <Plans />} /> */}
-        </Switch>
-      </BrowserRouter>
+      // <BrowserRouter>
+      // <Switch>
+      <Router history={history}>
+        <Route path="/" exact render={(props) => (<div>Helloworld</div>)} />
+        {/* <Route exact path="/react_app" render={() => "react app > Home!"} /> */}
+        {/* <Route exact path="/login" render={() => <Login />} /> */}
+        {/* <Route exact path="/features" render={() => <Features />} /> */}
+        {/* <Route exact path="/tariff_plans" render={() => <Plans />} /> */}
+      </Router>
+      // </Switch>
+      // </BrowserRouter>
     );
   }
 }
+
+App.propTypes = {
+  token: PropTypes.string,
+};
 
 export default App
