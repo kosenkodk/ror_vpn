d4a342f8d01ae60c8957debfeebe0756eb2cdad4
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-09-20 20:26:32 +0300

    App.js class instead of const

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,30 +1,37 @@
 import React from 'react'
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 import { Route } from 'react-router-dom'
 import Login from './Login'
 import Features from './Features'
 import Router from './Router'
 
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-//   }
-//   render() {
-//     return (
-//
-//     );
-//   }
-// }
-// App.propTypes = {
-//   token: PropTypes.string,
-// };
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-const App = props => (
-  <Router path={props.path} context={props}>
-    <Route path="/" exact render={(props) => (<div>token: {props.token}</div>)} />
-    <Route exact path="/login" render={() => <Login />} />
-    <Route exact path="/features" render={() => <Features />} />
-  </Router>
-)
+  render() {
+    return (
+      <Router path={this.props.path} context={{}}>
+        <Route path="/" exact render={(props) => (<div>token: {this.props.token}</div>)} />
+        <Route exact path="/login" render={() => <Login />} />
+        <Route exact path="/features" render={() => <Features />} />
+      </Router>
+    );
+  }
+}
+
+App.propTypes = {
+  token: PropTypes.string,
+  path: PropTypes.string,
+};
+
+// const App = props => (
+//   <Router path={props.path} context={props}>
+//     <Route path="/" exact render={(props) => (<div>token: {props.token}</div>)} />
+//     <Route exact path="/login" render={() => <Login />} />
+//     <Route exact path="/features" render={() => <Features />} />
+//   </Router>
+// )
 
 export default App
