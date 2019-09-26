d67b544f89f4810c40c008e7c0cf178126b83f44
Dmitry K <kosenkodk@gmail.com>
2019-09-26 20:41:53 +0300

    footer social links react component

--- app/javascript/components/sections/FooterSection.js
+++ app/javascript/components/sections/FooterSection.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Footer from './Footer'
-import FooterAccordion from './FooterAccordion'
+import Footer from '../Footer'
+import FooterAccordion from '../FooterAccordion'
 
 class FooterSection extends React.Component {
   render() {
