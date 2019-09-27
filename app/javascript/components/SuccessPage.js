0b9ffd8dfaf4363f8d2ab78c92d5a404922735da
Dmitry K <kosenkodk@gmail.com>
2019-09-27 14:58:19 +0300

    Success page. Added some images.

--- app/javascript/components/SuccessPage.js
+++ app/javascript/components/SuccessPage.js
@@ -1,10 +1,47 @@
 import React from 'react'
 import I18n from 'i18n-js/index.js.erb'
+import astronautImage from 'images/coming_soon/astronaut'
+import starImage from 'images/star'
+// import spaceshipImage from 'images/coming_soon/spaceship@2x.svg'
+import marsWithSpaceshipImage from 'images/coming_soon/mars_with_spaceship'
 
 class SuccessPage extends React.Component {
   render() {
     return (
-      <div className="container reset_pwd">
+
+      <div class="container" >
+        <div class="featurette">
+          <div class="row status_page text-center">
+            <div class="col-md-4">
+              <img src={astronautImage} className="img-fluid" alt="" />
+            </div>
+            <div class="col-md-4">
+              <h1 class="featurette-heading">
+
+              </h1>
+              <h2 class="featurette-heading">
+                {I18n.t('pages.reset_pwd.success.title')}
+
+                {/* {I18n.t('pages.reset_pwd.success.subtitle')} */}
+              </h2>
+              <p class="lead">
+                {I18n.t('pages.reset_pwd.success.subtitle')}
+
+                {/* <%=flash[:error] if flash[:error]%>
+          <%=flash[:notice] if flash[:notice]%> */}
+              </p>
+              <a href="/" class="btn btn-outline-primary btn-block">
+                <span class="glyphicon glyphicon-circle-arrow-right ">Ok</span></a>
+            </div>
+            <div class="col-md-4 justify-content-center align-self-center">
+              <img src={starImage} className="img-fluid" alt="" />
+            </div>
+            <div class="col-md-12">
+              <img src={marsWithSpaceshipImage} className="img-fluid" alt="" />
+            </div>
+          </div>
+        </div>
+        {/*<div className="container reset_pwd">
         <div className="featurette text-center">
           <div className="row">
             <div className="col-md-4 offset-md-4 text-center">
@@ -18,6 +55,7 @@ class SuccessPage extends React.Component {
             </div>
           </div>
         </div>
+    </div>*/}
       </div>
     )
   }
