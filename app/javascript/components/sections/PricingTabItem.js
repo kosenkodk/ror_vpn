275f47d5336a0dcb630596e7f5e71d8a8970630a
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-09-27 18:23:00 +0300

    Pricing Tab Section.

--- app/javascript/components/sections/PricingTabItem.js
+++ app/javascript/components/sections/PricingTabItem.js
@@ -1,73 +1,55 @@
 import React from 'react'
 import I18n from 'i18n-js/index.js.erb'
+import icon from 'images/icons/icon_checkbox_on'
 
 class PricingTabItem extends React.Component {
 
   render() {
-    // const { item } = this.props;
+    const { item } = this.props;
 
     return (
       <React.Fragment>
-        <h1>{this.props.item.title}</h1>
-        {/*<div class="card mb-3 active">
-          <!-- <div class="card-header">
-          </div> -->
-  <div class="card-body">
-            <% if item.price > 0 %>
-    <h1 class="card-title pricing-card-title pt-0 mt-0 mb-0">
-              <span class="text-sm-1 align-text-top">$</span><%= item.try(:price) %>
-    </h1>
-            <% else %>
-    <h1 class="card-title pricing-card-title">Free</h1>
-            <% end %>
-    <span class="text-sm-1 align-text-top pt-n5">Per month</span>
+        <div class="card mb-3 active">
+          {/* <!-- <div class="card-header">
+          </div> --> */}
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title pt-0 mt-0 mb-0">
+              <span class="text-sm-1 align-text-top">$</span> {item.price}
+            </h1>
+
+            <span class="text-sm-1 align-text-top pt-n5">Per month</span>
             <h5 class="card-title"></h5>
             <div class="row">
-              <% if item.price > 0 %>
-      <div class="col-md-6 offset-0">
+              <div class="col-md-6 offset-0">
                 <ul class="text-left list-unstyled">
-                  <% item.features.split(',').each do |feature| %>
-          <li><%= image_tag 'icons/icon_checkbox_on.png', {class: 'img-fluid', alt: '' } %> <%= feature %></li>
-                  <% end %>
-        </ul>
+                  {
+                    item.features.split(',').map(feature => (
+                      <li><img src={icon} class='img-fluid' alt='' /> {feature} </li>
+                    ))
+                  }
+                </ul>
               </div>
               <div class="col-md-6 offset-0 text-right">
                 <p class="pb-0">
-                  <%= link_to "Save $ #{item.try(:price_duration_sale)}", '#', {class: "btn btn-blue active rounded-pill text-white"} %>
-        </p>
-                <h5 class="card-title text-info"><strike>$ <%= item.try(:price_duration) %></strike></h5>
-                <h5 class="card-title"><%= item.try(:price_comment) %></h5>
+                  <a className="btn btn-blue active rounded-pill text-white">{`Save $ ${item.price_duration_sale} `} </a>
+                </p>
+                <h5 class="card-title text-info"><strike>$ {item.price_duration}</strike></h5>
+                <h5 class="card-title">{item.price_comment}</h5>
               </div>
               <div class="col-md-12 pt-2">
-                <!-- <button type="button" class="btn btn-outline-primary rounded-pill mb-n9">Best offer</button> -->
-        <%= link_to 'Start my free trial', '#', {class: 'btn btn-outline-primary'} %>
-      </div>
-              <% else %>
-      <% featureCollection1, featureCollection2 = devide_on_two_collections item.features.split(',') %>
-      <div class="col-md-4 offset-0">
-                <ul class="text-left list-unstyled">
-                  <% featureCollection1.each do |feature| %>
-          <li><%= image_tag 'icons/icon_checkbox_on.png', {class: 'img-fluid', alt: '' } %> <%= feature %></li>
-                  <% end %>
-        </ul>
+                {/* <!-- <button type="button" class="btn btn-outline-primary rounded-pill mb-n9">Best offer</button> --> */}
+                <a className="btn btn-outline-primary">
+                  Start my free trial
+                </a>
               </div>
-              <div class="col-md-4"></div>
-              <div class="col-md-4 offset-0">
-                <ul class="text-left list-unstyled">
-                  <% featureCollection2.each do |feature| %>
-          <li><%= image_tag 'icons/icon_checkbox_on.png', {class: 'img-fluid', alt: '' } %> <%= feature %></li>
-                  <% end %>
-        </ul>
-              </div>
-              <% end %>
-    </div>
+
+            </div>
           </div>
 
-          <!-- <div class="card-footer">
-          </div> -->
-</div>
-    */}
-      </React.Fragment >
+          {/* <!-- <div class="card-footer">
+          </div> --> */}
+        </div>
+      </React.Fragment>
     )
   }
 
---
+++ app/javascript/components/sections/PricingTabItemFree.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import I18n from 'i18n-js/index.js.erb'
+import icon from 'images/icons/icon_checkbox_on'
+
+class PricingTabItemFree extends React.Component {
+
+  render() {
+    const { item } = this.props;
+    let features = item.features.split(',')
+    let sizeHalf = features.length > 0 ? features.length / 2 : 0
+
+    let featureList = features.slice(0, sizeHalf)
+    let featureList2 = features.slice(sizeHalf, item.features.length)
+
+    return (
+      <React.Fragment>
+        <div class="card mb-3 active">
+          {/* <!-- <div class="card-header">
+          </div> --> */}
+          <div class="card-body">
+
+            <h1 class="card-title pricing-card-title">Free</h1>
+            <span class="text-sm-1 align-text-top pt-n5">Per month</span>
+            <h5 class="card-title"></h5>
+            <div class="row">
+
+              <div class="col-md-4 offset-0">
+                <ul class="text-left list-unstyled">
+                  {
+                    featureList.map(feature => (
+                      <li><img src={icon} class='img-fluid' alt='' /> {feature} </li>
+                    ))
+                  }
+                </ul>
+              </div>
+              <div class="col-md-4"></div>
+              <div class="col-md-4 offset-0">
+                <ul class="text-left list-unstyled">
+                  {
+                    featureList2.map(feature => (
+                      <li><img src={icon} class='img-fluid' alt='' /> {feature} </li>
+                    ))
+                  }
+                </ul>
+              </div>
+
+            </div>
+          </div>
+
+          {/* <!-- <div class="card-footer">
+          </div> --> */}
+        </div>
+      </React.Fragment >
+    )
+  }
+
+}
+export default PricingTabItemFree
