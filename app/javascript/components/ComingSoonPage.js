145148af66845b99a625ffd4152e6a4ee45941af
Dmitry K <kosenkodk@gmail.com>
2019-09-27 12:10:43 +0300

    Warning: Invalid DOM property `class`. Did you mean `className`?

--- app/javascript/components/ComingSoonPage.js
+++ app/javascript/components/ComingSoonPage.js
@@ -8,31 +8,31 @@ import marsWithSpaceshipImage from 'images/coming_soon/mars_with_spaceship'
 class ComingSoonPage extends React.Component {
   render() {
     return (
-      <div class="container status_page ">
-        <div class="featurette text-center">
-          <div class="row">
-            <div class="col-md-4">
-              <img src={astronautImage} class="img-fluid" />
+      <div className="container status_page ">
+        <div className="featurette text-center">
+          <div className="row">
+            <div className="col-md-4">
+              <img src={astronautImage} className="img-fluid" />
             </div>
-            <div class="col-md-4">
-              <h1 class="featurette-heading">
+            <div className="col-md-4">
+              <h1 className="featurette-heading">
                 {I18n.t('pages.coming_soon.title')}
               </h1>
               <br /><br /><br />
-              <h2 class="featurette-heading">
+              <h2 className="featurette-heading">
                 {I18n.t('pages.coming_soon.subtitle')}
               </h2>
-              <p class="lead">{I18n.t('pages.coming_soon.text')}</p>
-              {/* <!-- <a href="<%=root_path%>" class="btn btn-outline-primary">
-                <span class="glyphicon glyphicon-circle-arrow-right">{I18n.t('buttons.back_to_home_page')}</span>
+              <p className="lead">{I18n.t('pages.coming_soon.text')}</p>
+              {/* <!-- <a href="<%=root_path%>" className="btn btn-outline-primary">
+                <span className="glyphicon glyphicon-circle-arrow-right">{I18n.t('buttons.back_to_home_page')}</span>
               </a> --> */}
             </div>
-            <div class="col-md-4 justify-content-center align-self-center">
-              <img src={starImage} class="img-fluid" />
-              {/* <img src={spaceshipImage} class="img-fluid" /> */}
+            <div className="col-md-4 justify-content-center align-self-center">
+              <img src={starImage} className="img-fluid" />
+              {/* <img src={spaceshipImage} className="img-fluid" /> */}
             </div>
-            <div class="col-md-12">
-              <img src={marsWithSpaceshipImage} class="img-fluid" />
+            <div className="col-md-12">
+              <img src={marsWithSpaceshipImage} className="img-fluid" />
             </div>
           </div>
         </div>
