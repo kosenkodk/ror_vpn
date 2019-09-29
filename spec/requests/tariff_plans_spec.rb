d746fdf6c24d9d2bb111830917c858c11f27fdfc
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-09-29 19:27:04 +0300

    fix tarif plan url in spe

--- spec/requests/tariff_plans_spec.rb
+++ spec/requests/tariff_plans_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "TariffPlans", type: :request do
   describe "GET /tariff_plans" do
     it "works! (now write some real specs)" do
-      get tariff_plans_path
+      get api_v1_tariff_plans_path
       expect(response).to have_http_status(200)
     end
   end
