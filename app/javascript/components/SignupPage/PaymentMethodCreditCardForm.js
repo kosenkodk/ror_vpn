a14b42dcda05bc04bb34829db61b828be0dd0d5c
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-09-29 13:21:35 +0300

    Credit card section (html markup)

--- app/javascript/components/SignupPage/PaymentMethodCreditCardForm.js
+++ app/javascript/components/SignupPage/PaymentMethodCreditCardForm.js
@@ -15,38 +15,43 @@ class PaymentMethodCreditCardForm extends React.Component {
 
   render() {
     return (
-      <div className="row p-2 p-md-5 bg_credit_card">
+      <div className="row p-md-4 p-lg-5 bg_credit_card">
         <div className="col-sm-8">
           <h5>{I18n.t('bank_card.info')}</h5>
           <label className="col-form-label">{I18n.t('bank_card.number')}</label>
         </div>
-        <div className="col-sm-2"></div>
 
-        <div className="col-sm-8">
+        <div className="col-sm-8  ">
           <input type="email" required={true} className="form-control" ref={(input) => { this.email = input }} placeholder={I18n.t('bank_card.help.number')} />
         </div>
-        <div className="col-sm-2"></div>
+
+        <div className="col-sm-8 col-md-8">
+          <div className="row">
+            <div className="col-sm-6">
+              <label className="col-form-label">{I18n.t('bank_card.holder_name')}</label>
+              <input type="text" required={false} className="form-control" ref={(input) => { this.holder_name = input }} placeholder={I18n.t('bank_card.help.holder_name')} />
+            </div>
+            <div className="col-sm-3">
+              <label className="col-form-label">{I18n.t('bank_card.month')}</label>
+              <input type="text" required={false} className="form-control" ref={(input) => { this.month = input }} placeholder={I18n.t('bank_card.help.month')} />
+            </div>
+            <div className="col-sm-3">
+              <label className="col-form-label">{I18n.t('bank_card.year')}</label>
+              <input type="text" required={false} className="form-control" ref={(input) => { this.year = input }} placeholder={I18n.t('bank_card.help.year')} />
+            </div>
+          </div>
+        </div>
 
         <div className="col-sm-12">
-          <label className="col-form-label">{I18n.t('bank_card.holder_name')}</label>
+          <div className="row">
+            <div className="col"></div>
+            <div className="col-sm-3 col-md-4 col-lg-3 text-black-50 ml-auto pt-n5">
+              <label className="col-form-label">{I18n.t('bank_card.cvvcvc')}</label>
+              <input type="text" required={false} className="form-control" ref={(input) => { this.cvc = input }} placeholder={I18n.t('bank_card.help.cvc')} />
+            </div>
+          </div>
         </div>
-        <div className="col-sm-4">
-          <input type="text" required={false} className="form-control" ref={(input) => { this.holder_name = input }} placeholder={I18n.t('bank_card.help.holder_name')} />
-        </div>
-
-        <div className="col-sm-2">
-          <input type="text" required={false} className="form-control" ref={(input) => { this.month = input }} placeholder={I18n.t('bank_card.help.month')} />
-        </div>
-
-        <div className="col-sm-2">
-          <input type="text" required={false} className="form-control" ref={(input) => { this.year = input }} placeholder={I18n.t('bank_card.help.year')} />
-        </div>
-
-        <div className="col-sm-4 text-black-50 pl-5 pr-1">
-          <label className="col-form-label">{I18n.t('bank_card.cvc')}</label>
-          <input type="text" required={false} className="form-control" ref={(input) => { this.cvc = input }} placeholder={I18n.t('bank_card.help.cvc')} />
-        </div>
-      </div>
+      </div >
     )
   }
 }
