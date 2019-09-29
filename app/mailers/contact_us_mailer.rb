ae634a91e0886cbfaee894ba10d52a0873987931
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-09-29 18:50:29 +0300

    Fix mail tests

--- app/mailers/contact_us_mailer.rb
+++ app/mailers/contact_us_mailer.rb
@@ -1,9 +1,9 @@
 class ContactUsMailer < ApplicationMailer
   def notify_user(email, contact_id)
     @contact_id = contact_id
-    mail to: email, subject: 'Congrats with your new message!', body: "contact link: #{contact_url(contact_id)}"
+    mail to: email, subject: 'Congrats with your new message!', body: "contact link: #{api_v1_contact_url(contact_id)}"
   end
   def notify_admin(email, contact_id)
-    mail to: email, subject: 'Contact Us. New message.', body: "contact link: #{contact_url(contact_id)}"
+    mail to: email, subject: 'Contact Us. New message.', body: "contact link: #{api_v1_contact_url(contact_id)}"
   end
 end
