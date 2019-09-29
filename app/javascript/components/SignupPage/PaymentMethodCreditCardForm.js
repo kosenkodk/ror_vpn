0c61d2ffe90bbae4063b3d205f7bd0dbaa7590ae
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-09-29 08:35:49 +0300

    Sign up > Payment method > Fix Warning: Invalid DOM property `class`. Did you mean `className`?

--- app/javascript/components/SignupPage/PaymentMethodCreditCardForm.js
+++ app/javascript/components/SignupPage/PaymentMethodCreditCardForm.js
@@ -15,34 +15,34 @@ class PaymentMethodCreditCardForm extends React.Component {
 
   render() {
     return (
-      <div class="row p-md-5 bg_credit_card">
-        <div class="col-sm-8">
+      <div className="row p-md-5 bg_credit_card">
+        <div className="col-sm-8">
           <h5>{I18n.t('bank_card.info')}</h5>
           <label className="col-form-label">{I18n.t('bank_card.number')}</label>
         </div>
-        <div class="col-sm-2"></div>
+        <div className="col-sm-2"></div>
 
-        <div class="col-sm-8">
+        <div className="col-sm-8">
           <input type="email" required={true} className="form-control" ref={(input) => { this.email = input }} placeholder={I18n.t('bank_card.help.number')} />
         </div>
-        <div class="col-sm-2"></div>
+        <div className="col-sm-2"></div>
 
-        <div class="col-sm-12">
+        <div className="col-sm-12">
           <label className="col-form-label">{I18n.t('bank_card.holder_name')}</label>
         </div>
-        <div class="col-sm-4">
+        <div className="col-sm-4">
           <input type="text" required={false} className="form-control" ref={(input) => { this.holder_name = input }} placeholder={I18n.t('bank_card.help.holder_name')} />
         </div>
 
-        <div class="col-sm-2">
+        <div className="col-sm-2">
           <input type="text" required={false} className="form-control" ref={(input) => { this.month = input }} placeholder={I18n.t('bank_card.help.month')} />
         </div>
 
-        <div class="col-sm-2">
+        <div className="col-sm-2">
           <input type="text" required={false} className="form-control" ref={(input) => { this.year = input }} placeholder={I18n.t('bank_card.help.year')} />
         </div>
 
-        <div class="col-sm-4 text-black-50 pl-5 pr-1">
+        <div className="col-sm-4 text-black-50 pl-5 pr-1">
           <label className="col-form-label">{I18n.t('bank_card.cvc')}</label>
           <input type="text" required={false} className="form-control" ref={(input) => { this.cvc = input }} placeholder={I18n.t('bank_card.help.cvc')} />
         </div>
