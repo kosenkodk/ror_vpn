d83ffe6996dc29f56a85b9c8fe6e3ce7660adc79
Dmitry K <kosenkodk@gmail.com>
2019-09-29 20:09:03 +0300

    Fix some urls in tests

--- spec/features/coming_soon_page_spec.rb
+++ spec/features/coming_soon_page_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe ErrorsController, type: :feature do
   describe 'Get Coming Soon Page' do
     it 'renders coming_soon template' do
-      visit(coming_soon_path)
+      visit('/coming_soon')
       expect(page).to have_content('Coming Soon')
       expect(page).to have_content('Our developers are currently working hard building this page')
     end
