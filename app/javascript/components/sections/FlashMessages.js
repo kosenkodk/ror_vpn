634a7c7e32f86313615b0bde7dcf8e6de1b40b97
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-09-30 17:18:17 +0300

    contact us form (send request and handle response message with status)

--- app/javascript/components/sections/FlashMessages.js
+++ app/javascript/components/sections/FlashMessages.js
@@ -3,17 +3,16 @@ import I18n from 'i18n-js/index.js.erb'
 
 class FlashMessages extends React.Component {
   render() {
-    let error = ''
-    let notice = ''
+    let { error, notice } = this.props
     return (
       <React.Fragment>
         {
-          error.length > 0 && <div class={`alert ${error ? "alert-danger" : ""}`} role="alert">
+          error.length > 0 && <div className={`alert ${error ? "alert-danger" : ""}`} role="alert">
             {error}
           </div>
         }
         {
-          notice.length > 0 && <div class={`alert ${notice ? "alert-success" : ""}`} role="alert">
+          notice.length > 0 && <div className={`alert ${notice ? "alert-success" : ""}`} role="alert">
             {notice}
           </div>
         }
