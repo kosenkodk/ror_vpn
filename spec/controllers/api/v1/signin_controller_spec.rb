fff1418d31765017cd5edb14b4567b4ab62fd538
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-10-01 15:59:25 +0300

    Specs

--- spec/controllers/api/v1/signin_controller_spec.rb
+++ spec/controllers/api/v1/signin_controller_spec.rb
@@ -1,5 +1,70 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::SigninController, type: :controller do
+  let(:user) { FactoryBot.create(:user) }
+  
+  describe 'POST #create' do
+
+    let(:user_params) { { email: user.email, password: user.password } }
+
+    context 'login' do
+      it 'returns http success' do
+        post :create, params: user_params
+        expect(response).to be_successful
+        expect(response_json.keys).to eq(['csrf'])
+        expect(response.cookies[JWTSessions.access_cookie]).to be_present
+      end
+
+      it 'returns unathorized status for invalid params' do
+        post :create, params: { email: user.email, password: 'incorrect' }
+        expect(response.status).to eq(401)
+      end
+    end
+
+    context 'logout' do
+
+      let(:access_cookie) { @tokens[:access] }
+      let(:csrf_token) { @tokens[:csrf] }
+
+      before do
+        # set expiration time to 0 to create an already expired access token
+        # JWTSessions.access_exp_time = 0
+        payload = { user_id: user.id }
+        session = JWTSessions::Session.new(payload: payload, refresh_by_access_allowed: true)
+        @tokens = session.login
+        JWTSessions.access_exp_time = 3600
+      end
+     
+      it 'success' do
+        request.cookies[JWTSessions.access_cookie] = access_cookie
+        request.headers[JWTSessions.csrf_header] = csrf_token
+
+        # # use it in the web client requests
+        # headers = {
+        #   'ACCEPT': 'application/json',     # This is what Rails 4 accepts
+        #   # 'HTTP_ACCEPT': 'application/json', # This is what Rails 3 accepts
+        #   'X-CSRF-Token': csrf_token,
+        #   # 'X-Refresh-Token': '',
+        #   'Authorization': 'Bearer #{access_cookie}'
+        # }
+        # request.headers.merge! headers
+        
+        delete :destroy
+
+        expect(response.content_type).to eq('application/json; charset=utf-8')
+        expect(response.cookies[JWTSessions.access_cookie]).not_to be_present
+        expect(response_json).to eq('ok')
+        expect(response).to be_successful
+      end
+      it 'fail' do
+        delete :destroy
+        
+        expect(response_json.keys).not_to eq(['csrf'])
+        expect(response.cookies[JWTSessions.access_cookie]).not_to be_present
+        expect(response_json.values).to eq(['Not authorized'])
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+  end
 
 end
--- spec/controllers/api/v1/todos_controller_spec.rb
+++ spec/controllers/api/v1/todos_controller_spec.rb
@@ -1,5 +1,115 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::TodosController, type: :controller do
+  let(:user) { create(:user) }
 
+  let(:valid_attributes) {
+    { title: 'new title' }
+  }
+
+  let(:invalid_attributes) {
+    { title: nil }
+  }
+
+  before { sign_in_as(user) }
+
+  describe 'GET #index' do
+    let!(:todo) { create(:todo, user: user) }
+
+    it 'returns a success response' do
+      get :index
+      expect(response).to be_successful
+      expect(response_json.size).to eq 1
+      expect(response_json.first['id']).to eq todo.id
+    end
+
+    # usually there's no need to test this kind of stuff, it's here for the presentation purpose
+    it 'unauth without cookie' do
+      request.cookies[JWTSessions.access_cookie] = nil
+      get :index
+      expect(response).to have_http_status(401)
+    end
+  end
+
+  describe 'GET #show' do
+    let!(:todo) { create(:todo, user: user) }
+    before { sign_in_as(user) }
+
+    it 'returns a success response' do
+      get :show, params: { id: todo.id }
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'POST #create' do
+
+    context 'with valid params' do
+      it 'creates a new Todo' do
+        expect {
+          post :create, params: { todo: valid_attributes }
+        }.to change(Todo, :count).by(1)
+      end
+
+      it 'renders a JSON response with the new todo' do
+        post :create, params: { todo: valid_attributes }
+        expect(response).to have_http_status(:created)
+        expect(response.content_type).to eq('application/json; charset=utf-8')
+        expect(response.location).to eq(todo_url(Todo.last))
+      end
+
+      it 'unauth without CSRF' do
+        request.headers[JWTSessions.csrf_header] = nil
+        post :create, params: { todo: valid_attributes }
+        expect(response).to have_http_status(401)
+      end
+    end
+
+    context 'with invalid params' do
+      it 'renders a JSON response with errors for the new todo' do
+        post :create, params: { todo: invalid_attributes }
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.content_type).to eq('application/json; charset=utf-8')
+      end
+    end
+  end
+
+  describe 'PUT #update' do
+    let!(:todo) { create(:todo, user: user) }
+
+    context 'with valid params' do
+      let(:new_attributes) {
+        { title: 'Super secret title' }
+      }
+
+      it 'updates the requested todo' do
+        put :update, params: { id: todo.id, todo: new_attributes }
+        todo.reload
+        expect(todo.title).to eq new_attributes[:title]
+      end
+
+      it 'renders a JSON response with the todo' do
+        put :update, params: { id: todo.to_param, todo: valid_attributes }
+        expect(response).to have_http_status(:ok)
+        expect(response.content_type).to eq('application/json; charset=utf-8')
+      end
+    end
+
+    context 'with invalid params' do
+      it 'renders a JSON response with errors for the todo' do
+        put :update, params: { id: todo.to_param, todo: invalid_attributes }
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.content_type).to eq('application/json; charset=utf-8')
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    let!(:todo) { create(:todo, user: user) }
+
+    it 'destroys the requested todo' do
+      expect {
+        delete :destroy, params: { id: todo.id }
+      }.to change(Todo, :count).by(-1)
+    end
+  end
 end
