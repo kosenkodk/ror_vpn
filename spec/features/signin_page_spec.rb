6085d36b44d481b854620a6a8c67d3d8a2e0a190
Dmitry K <kosenkodk@gmail.com>
2019-10-01 18:34:26 +0300

    Sign in. Pending test with valid data.

--- spec/features/signin_page_spec.rb
+++ spec/features/signin_page_spec.rb
@@ -3,16 +3,20 @@ require 'rails_helper'
 RSpec.describe AuthController, type: :feature, js: true do
 
   describe 'GET :signin' do
+    
     before do 
       visit('/login')
     end
 
-    context 'invalid data' do
-      it 'render :login template' do
+    context 'error' do
+      it 'with invalid credentials' do
         click_on('Submit')
         expect(find('.alert')).to have_text(I18n.t('signin.invalid_credentials'))
       end
     end
 
+    context 'success' do
+      it 'with valid credentials'
+    end
   end
 end
