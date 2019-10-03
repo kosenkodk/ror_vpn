581e63daf885fc1ef0cb51c89b678ede7e189079
Dmitry K <kosenkodk@gmail.com>
2019-10-03 18:07:32 +0300

    Tickets with Sidebar.

---
+++ app/javascript/components/Routes.js
@@ -0,0 +1,14 @@
+export const Routes = {
+
+  'tickets': {
+    path: '/tickets',
+    name: 'Tickets',
+    component: ''
+  },
+  'coming_soon': {
+    path: '/404',
+    name: 'Coming Soon',
+    component: ''
+  },
+
+};
