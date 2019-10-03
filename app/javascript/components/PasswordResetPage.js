187bce134cabedc3f9b400cd07580197bf20e20d
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-10-03 15:26:22 +0300

    Password Reset Page. Alerts and send request to the api.

--- app/javascript/components/PasswordResetPage.js
+++ app/javascript/components/PasswordResetPage.js
@@ -1,17 +1,39 @@
 import React from 'react'
 import I18n from 'i18n-js/index.js.erb'
 import PasswordResetForm from './PasswordResetForm'
+import { postCsrfRequest, handleErrors } from 'helpers/http'
 
 class PasswordResetPage extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      error: '',
+      notice: ''
+    }
     this.handleFormSubmit = this.handleFormSubmit.bind(this)
   }
 
-  handleFormSubmit(e, email, password) {
-    console.log(email, password)
+  handleFormSubmit(e, password, password_confirm) {
     e.preventDefault()
+    console.log(password, password_confirm)
+    const data = { 'password': password, 'password_confirm': password_confirm }
+
+    fetch(postCsrfRequest('/api/v1/password_resets', 'PATCH', data))
+      .then(handleErrors)
+      .then((item, message) => {
+        console.log('success', item, message)
+        this.setState({ error: '' })
+        this.setState({ notice: item.message })
+        // this.props.history.push('/reset_ok')
+        // this.props.history.push('/200')
+      })
+      .catch((error) => {
+        console.log('error', error.message)
+        this.setState({ error: error.message })
+        // this.setState({ error: response.statusText })
+      });
+
   }
 
   render() {
@@ -26,6 +48,10 @@ class PasswordResetPage extends React.Component {
               <p className="lead">
                 {I18n.t('pages.reset_pwd.subtitle')}
               </p>
+            </div>
+
+            <div className="col-sm-4 offset-md-4 text-center">
+              <FlashMessages error={this.state.error} notice={this.state.notice} />
             </div>
 
             <div className="col-md-8 offset-md-2">
