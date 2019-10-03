229313d35653b5538ae7163af30e2ac5eaf979ea
Dmitry K <kosenkodk@gmail.com>
2019-10-03 14:06:12 +0300

    Sign in specs (checking links)

--- spec/features/signin_page_spec.rb
+++ spec/features/signin_page_spec.rb
@@ -18,5 +18,20 @@ RSpec.describe AuthController, type: :feature, js: true do
     context 'success' do
       it 'with valid credentials'
     end
+
+    context 'checking links on' do
+      it 'sign up page' do
+        click_on(I18n.t("pages.signup.title")) # Don't have account link
+        expect(page).to have_css('.signup')
+      end
+      it 'forgot password page' do
+        click_on(I18n.t('pages.login.form.forgot_pwd'))
+        expect(page).to have_css('.forgot_pwd')
+      end
+      it 'forgot password page' do
+        click_on(I18n.t("pages.login.form.login_trouble"))
+        expect(page).to have_css('.forgot_pwd')
+      end
+    end
   end
 end
