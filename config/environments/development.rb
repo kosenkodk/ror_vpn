8b779b12ad0e3bc21d0abf644d2b4d291d06d653
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-10-04 12:01:10 +0300

    Displaying email in console logs in dev mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,9 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.logger = true
+  config.log_level = :debug
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
