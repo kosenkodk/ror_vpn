ac17e5366a4e06368a0d64cb89d484d0b99700aa
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-10-07 11:25:26 +0300

    Fix text inputs and file field: Warning: Invalid DOM property `for`. Did you mean `htmlFor`?

--- app/javascript/components/TicketsPage/TicketViewModal.js
+++ app/javascript/components/TicketsPage/TicketViewModal.js
@@ -26,7 +26,7 @@ class TicketViewModal extends React.Component {
           <div className="modal-dialog" role="document">
             <div className="modal-content">
               <div className="modal-header">
-                <h5 className="modal-title" id={idModalTitle}>{this.props.title}</h5>
+                <h5 className="modal-title" id={idModalTitle}>{this.props.title || ''}</h5>
                 <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
