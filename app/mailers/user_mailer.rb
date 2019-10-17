ba25263658f80a52da2a1a5f6fcbccc8ce5f93c0
Dmitry K <kosenkodk@gmail.com>
2019-10-17 21:13:53 +0300

    user mailer for signup

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,4 +3,8 @@ class UserMailer < ApplicationMailer
     @user = user
     make_bootstrap_mail(to: @user.email, subject: 'Reset your password')
   end
+  def signup(user)
+    @user = user
+    make_bootstrap_mail(to: @user.email, subject: 'Thank you for creating a VegaVPN account.')
+  end
 end
