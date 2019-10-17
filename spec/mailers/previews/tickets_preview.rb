0bf62316d2620c4954927d837410dd0fd562fa6b
Dmitry K <kosenkodk@gmail.com>
2019-10-17 19:33:35 +0300

    Preview tickets mailer

---
+++ spec/mailers/previews/tickets_preview.rb
@@ -0,0 +1,9 @@
+# Preview all emails at http://localhost:3000/rails/mailers/contact_us
+class TicketsPreview < ActionMailer::Preview
+  def notify_user_from
+    TicketsMailer.notify_user_from('', '', Ticket.new(title:'title', text:'text', user: User.new(email:'email@ex.com'), department: Department.new(title:'department')))
+  end
+  def notify_department_from
+    TicketsMailer.notify_department_from('', '', Ticket.new(title:'title', text:'text', user: User.new(email:'email@ex.com'), department: Department.new(title:'department')))
+  end
+end
