bddb76cdb122e2ed52eb735283de6c7324d4674f
Dmitry K <kosenkodk@gmail.com>
2019-10-17 14:35:07 +0300

    notify user and department

--- app/mailers/contact_us_mailer.rb
+++ app/mailers/contact_us_mailer.rb
@@ -3,7 +3,15 @@ class ContactUsMailer < ApplicationMailer
     @contact_id = contact_id
     mail to: email, subject: 'Congrats with your new message!', body: "contact link: #{api_v1_contact_url(contact_id)}"
   end
+  def notify_user_from(email_from, email_to, contact)
+    @contact = contact
+    mail from: email_from, to: email_to, subject: 'Congrats with your new message!'#, body: "contact link: #{api_v1_contact_url(@contact.id)}"
+  end
   def notify_admin(email, contact_id)
     mail to: email, subject: 'Contact Us. New message.', body: "contact link: #{api_v1_contact_url(contact_id)}"
+  end
+  def notify_department_from(email_from, email_to, contact)
+    @contact = contact
+    mail from: email_from, to: email_to, subject: 'Contact Us. New message.', body: "contact link: #{api_v1_contact_url(@contact.id)}"
   end
 end
