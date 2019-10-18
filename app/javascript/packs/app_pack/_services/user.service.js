df1cbf2202b83ca71ea3a82434e620693c5fd922
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-10-18 22:40:33 +0300

    trying to refresh token and retry request on the fly

--- app/javascript/packs/app_pack/_services/user.service.js
+++ app/javascript/packs/app_pack/_services/user.service.js
@@ -1,5 +1,6 @@
 import { config } from 'config';
-import { authHeader } from '../_helpers';
+import { history, authHeader } from '../_helpers';
+// import { handleErrors } from '../../../helpers/http';
 
 export const userService = {
   login,
@@ -32,13 +33,17 @@ function login(email, password) {
 
       localStorage.setItem('csrf', JSON.stringify(response.csrf));
 
-      return fetch(`${config.apiUrl}/me`, { method: 'GET', headers: authHeader() })
-        .then(handleResponse)
-        .then(user => {
-          // store user details and jwt token in local storage to keep user logged in between page refreshes
-          localStorage.setItem('user', JSON.stringify(user));
-          return user;
-        });
+      return getUser()
+    });
+}
+
+function getUser() {
+  return fetch(`${config.apiUrl}/me`, { method: 'GET', headers: authHeader() })
+    .then(handleResponse)
+    .then(user => {
+      // store user details and jwt token in local storage to keep user logged in between page refreshes
+      localStorage.setItem('user', JSON.stringify(user));
+      return user;
     });
 }
 
@@ -46,9 +51,12 @@ function logout() {
   // remove user from local storage to log user out
   localStorage.removeItem('user');
   localStorage.removeItem('csrf');
+  history.push(config.urlAfterSignout)
 }
 
 function getAll() {
+  return sendRequestAndRetryByUrlMethodData(`${config.apiUrl}/users`, 'GET', {})
+
   const requestOptions = {
     method: 'GET',
     headers: authHeader()
@@ -58,6 +66,8 @@ function getAll() {
 }
 
 function contactUs(contact) {
+  return sendRequestAndRetryByUrlMethodData(`${config.apiUrl}/contacts`, 'POST', { contact: contact })
+
   const requestOptions = {
     method: 'POST',
     headers: authHeader(),
@@ -68,6 +78,8 @@ function contactUs(contact) {
 }
 
 function addTicket(ticket) {
+  return sendRequestAndRetryByUrlMethodData(`${config.apiUrl}/tickets`, 'POST', { ticket })
+
   const requestOptions = {
     method: 'POST',
     headers: authHeader(),
@@ -77,6 +89,8 @@ function addTicket(ticket) {
 }
 
 function viewTicket(id) {
+  return sendRequestAndRetryByUrlMethodData(`${config.apiUrl}/tickets/${id}`, 'GET', {})
+
   const requestOptions = {
     method: 'GET',
     headers: authHeader(),
@@ -85,6 +99,8 @@ function viewTicket(id) {
 }
 
 function getTickets() {
+  return sendRequestAndRetryByUrlMethodData(`${config.apiUrl}/tickets`, 'GET', {})
+
   const requestOptions = {
     method: 'GET',
     headers: authHeader()
@@ -93,11 +109,76 @@ function getTickets() {
 }
 
 function getDepartments() {
+  return sendRequestAndRetryByUrlMethodData(`${config.apiUrl}/departments`, 'GET', {})
+
   const requestOptions = {
     method: 'GET',
     headers: authHeader()
   }
   return fetch(`${config.apiUrl}/departments`, requestOptions).then(handleResponse);
+}
+
+function sendRequestAndRetryByUrlMethodData(url, method, data) {
+  let requestOptions = {
+    method: method,
+    headers: authHeader(),
+    body: JSON.stringify(data),
+  }
+  if (method === 'GET')
+    requestOptions = {
+      method: method,
+      headers: authHeader(),
+    }
+
+  return fetch(url, requestOptions).then(refreshAndRetry)
+}
+
+function refreshAndRetry(response) {
+  return response.text()
+    .then(text => {
+      let data = {}
+      try {
+        data = JSON.parse(text);
+      } catch (e) { }
+
+      // network error
+      if (!response.ok) {
+        if (response.status === 401) {
+          return handle401Status(url, method, data)
+        }
+
+        const error = (data && data.error) || (data && data.message) || response.statusText;
+        return Promise.reject(error);
+      }
+
+      // api error
+      const error = (data && data.error) || (data && data.message) || response.statusText;
+      if (data && data.error) return Promise.reject(error)
+
+      return data;
+    });
+}
+
+function handle401Status(url, method, data) {
+  const requestOptions = {
+    method: 'POST',
+    headers: authHeader()
+  }
+  return fetch(`${config.apiUrl}/refresh`, requestOptions).then(handleResponse)
+    .then(response => {
+      localStorage.setItem('csrf', JSON.stringify(response.csrf))
+      getUser()
+      // retrying request with a new refreshed csrf token
+      const requestOptions = {
+        method: method,
+        headers: authHeader(),
+        body: JSON.stringify(data)
+      }
+      return fetch(url, requestOptions).then(handleResponse)
+    }).catch(error => {
+      logout()
+      return Promise.reject(error);
+    });
 }
 
 function handleResponse(response) {
@@ -126,3 +207,4 @@ function handleResponse(response) {
     return data;
   });
 }
+
