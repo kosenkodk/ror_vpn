c637303768575030b9c805e44825347eb2fd5c93
Dmitry K <kosenkodk@gmail.com>
2019-10-20 11:22:32 +0300

    Pending test (add ticket with comment)

--- spec/features/tickets_page_spec.rb
+++ spec/features/tickets_page_spec.rb
@@ -1,22 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1:TicketsController', type: :feature, js: true do
+  let!(:user) {create(:user, role: 0)}
+  let!(:ticket) {create(:ticket, user: user)}
+  let!(:department_billing) {create(:department, title: 'Billing')}
+  let!(:department_sales) {create(:department, title: 'Sales')}
+  let!(:department_tech) {create(:department, title: 'Technical Support')}
+
+  before(:each) do
+    fsign_in_as(user)
+    # visit('/tickets')
+  end
+
+  describe 'view ticket' do
+    context 'success' do
+      xit 'add comments' do
+        click_on(I18n.t('buttons.add'))
+        fill_in :title, with: 'ticket with comment'
+        fill_in :comment, with: 'comment'
+        click_on(I18n.t('buttons.submit'))
+        click_on('ticket with comment')
+        expect(page).to have_content('ticket with comment')
+        expect(page).to have_content('comment')
+      end
+    end
+  end
+
   describe 'Tickets' do
-    let!(:user) {create(:user, role: 0)}
-    let!(:ticket) {create(:ticket, user: user)}
-    let!(:department_billing) {create(:department, title: 'Billing')}
-    let!(:department_sales) {create(:department, title: 'Sales')}
-    let!(:department_tech) {create(:department, title: 'Technical Support')}
     # let!(:ticket_billing) {create(:ticket, user: user, department: department_billing)}
     # let!(:ticket_sales) {create(:ticket, user: user, department: department_sales)}
     # let!(:ticket_tech) {create(:ticket, user: user, department: department_tech)}
 
     context 'user' do
-      before(:each) do
-        fsign_in_as(user)
-        # visit('/tickets')
-      end
-
       it 'list' do
         expect(page).to have_content(ticket.title)
       end
@@ -111,7 +126,7 @@ RSpec.describe 'Api::V1:TicketsController', type: :feature, js: true do
       it "display ticket's status" do
         expect(page).to have_content('opened')
       end
-
+      
       it "searching tickets by title or text"
 
       
@@ -119,7 +134,7 @@ RSpec.describe 'Api::V1:TicketsController', type: :feature, js: true do
       it "pagination click on next page button"
       it "pagination click on prev page button"
       it "title can't be empty"
-      it "view ticket > add comments"
+      
       it "admins can edit and delete tickets - why delete ? - just change ticket's status"
       it "email user and selected department about ticket's status or new comment"
     end
