4702c2d433380cd48f19ab33c9637822e1fcdda6
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-10-22 22:49:51 +0300

    Trying to fix logo in email

--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -1,5 +1,14 @@
 module EmailHelper
   def email_image_tag(image, **options)
+    attachments[image] = {
+      data: File.read(Rails.root.join("app/assets/images/#{image}")),
+      mime_type: "image/png",
+      # encoding: "base64"
+    }
+    image_tag attachments[image].url, **options
+  end
+
+  def email_inline_image_tag(image, **options)
     attachments.inline[image] = File.read(Rails.root.join("app/assets/images/#{image}"))
     image_tag attachments.inline[image].url, **options
   end
