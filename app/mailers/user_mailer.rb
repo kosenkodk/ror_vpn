1b1a951f3371d1573f1de62402b5930da9a6d53b
Dmitry K <kosenkodk@gmail.com>
2019-10-22 22:22:37 +0300

    Fix logo in email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,6 @@
 class UserMailer < ApplicationMailer
   def reset_password(user)
+    # attachments.inline['logo_mail_black.png'] = File.read(File.join(Rails.root,'/app/assets/images/logo_mail_black.png'))
     @user = user
     make_bootstrap_mail(to: @user.email, subject: 'Reset your password')
   end
