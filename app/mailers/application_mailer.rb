e9ba2721ddd31f0c3b5bb26f05d86b61b0c6e687
Dmitry K <kosenkodk@gmail.com>
2019-10-23 19:45:07 +0300

    Improve ticket email texts

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
+  # include Rails.application.routes.url_helpers
   add_template_helper(EmailHelper)
   # default from: 'from@example.com'
   # layout 'mailer'
