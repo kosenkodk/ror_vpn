61c6ba322158b328cc1235aa5d3e87fd2b6d214d
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-10-23 21:24:21 +0300

    Fix ticket url in emails

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module VegaVPN
     config.hosts << 'localhost'
     config.hosts << '127.0.0.1'
     WillPaginate.per_page = 4
-
+    # config.routes.default_url_options = {host:'vega.isit.su'}
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
