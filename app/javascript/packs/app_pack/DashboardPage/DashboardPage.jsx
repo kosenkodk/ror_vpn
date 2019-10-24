1098ab760fe1672c2962dd69581a8bdf8cfb533e
Dmitry K <kosenkodk@gmail.com>
2019-10-24 19:56:55 +0300

    Dashboard page.

---
+++ app/javascript/packs/app_pack/DashboardPage/DashboardPage.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { userActions } from '../_actions';
+import I18n from 'i18n-js/index.js.erb'
+import Plans from '../SignupPage/Plans';
+
+class DashboardPage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { loggingIn } = this.props;
+    return (
+      <div className="col-md-6 col-md-offset-3">
+        <div id="plans">plans
+        <Plans />
+        </div>
+        <div id="subscriptions">subscription</div>
+        <div id="billing">billing</div>
+
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  const { loggingIn } = state.authentication;
+  return {
+    loggingIn
+  };
+}
+
+const connectedPage = connect(mapStateToProps)(DashboardPage);
+export { connectedPage as DashboardPage }; 
