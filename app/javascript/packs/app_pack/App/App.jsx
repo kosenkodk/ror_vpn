64858390dfda46226718731f4dee94b11bdef2d7
Dmitry K <kosenkodk@gmail.com>
2019-10-24 18:06:16 +0300

    Sidebar with default nav items

--- app/javascript/packs/app_pack/App/App.jsx
+++ app/javascript/packs/app_pack/App/App.jsx
@@ -86,7 +86,7 @@ class App extends React.Component {
                     <PrivateRoute exact path={urls.tickets_new.path} component={TicketsNewPage} />
                     <PrivateRoute exact path={urls.tickets_edit.path} component={TicketsEditPage} />
                     <PrivateRoute exact path={urls.tickets_view.path} component={TicketsViewPage} />s
-                    <PrivateRouteWithRightSidebar path={urls.user_dashboard.path} sidebarItems={[urls.user_plans, urls.user_subscriptions, urls.user_billing]} component={ComingSoonPage} />
+                    <PrivateRouteWithRightSidebar path={urls.user_dashboard.path} sidebarUrls={[urls.user_plans, urls.user_subscriptions, urls.user_billing]} component={ComingSoonPage} />
                     <PrivateRoute path={urls.user_payment.path} component={ComingSoonPage} />
                     <PrivateRoute path={urls.user_downloads.path} component={ComingSoonPage} />
                     <PrivateRoute path={urls.user_invite_friend.path} component={ComingSoonPage} />
