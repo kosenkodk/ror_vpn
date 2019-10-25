59ce83d14f75bc7b57b290a23cc2d296852bfdc9
Dmitry K <kosenkodk@gmail.com>
2019-10-25 18:34:17 +0300

    fix ticket specs

--- app/controllers/api/v1/tariff_plans_controller.rb
+++ app/controllers/api/v1/tariff_plans_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::TariffPlansController < Api::V1::ApiController
   def index
     items = TariffPlan.all
-    render json: items.as_json(methods: [:active_class])#.as_json(only: [:id, :title, :subtitle, :text], methods: [:icon_url])
+    render json: items.as_json(methods: [:active_class]) #.as_json(only: [:id, :title, :subtitle, :text], methods: [:icon_url])
   end
 
   def show
     item = TariffPlan.find(params[:id])
-    render json: item#.attributes.merge({icon_url: url_for(item.icon)})
+    render json: item #.attributes.merge({icon_url: url_for(item.icon)})
   end
 end
--- spec/requests/tariff_plans_spec.rb
+++ spec/requests/tariff_plans_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "TariffPlans", type: :request do
+RSpec.describe 'Api::V1::TariffPlansController', type: :request do
   describe "GET /tariff_plans" do
     it "works! (now write some real specs)" do
       get api_v1_tariff_plans_path
