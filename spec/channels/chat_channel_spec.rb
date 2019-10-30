8cd8a3fc1218eda5321af6b6bf8f83282c7b6076
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-10-30 18:03:04 +0300

    Transmission.last is empty

--- spec/channels/chat_channel_spec.rb
+++ spec/channels/chat_channel_spec.rb
@@ -1,13 +1,17 @@
 require 'rails_helper'
+
 RSpec.describe ChatChannel, type: :channel do
-  let(:user) {create(:user)}
-  let(:ticket) {create(:ticket, user: user)}
+  let!(:user) {create(:user)}
+  let!(:ticket) {create(:ticket, user: user)}
+  let!(:message) {create(:message, user: user, ticket_id: ticket.id)}
   let(:room) {"Room#{ticket.id}"}
   let(:ticket_channel) {"ticket_channel#{room}"}
-
+  let(:chat_channel) {"ChatChannel"}
+  
   before do
     # initialize connection with identifiers
     stub_connection current_user: user
+    # stub_connection channel: chat_channel
   end
 
   xit "rejects when no room id" do
@@ -17,7 +21,10 @@ RSpec.describe ChatChannel, type: :channel do
   end
 
   it "subscribes to a stream when room id is provided" do
-    subscribe(room: room)
+    # subscribe room: room
+    # subscribe(channel: chat_channel)
+    subscribe(channel: chat_channel, room: room)
+    # subscribe(channel: ticket_channel, room: room)
 
     expect(subscription).to be_confirmed
 
@@ -26,5 +33,11 @@ RSpec.describe ChatChannel, type: :channel do
 
     # or directly by model if you create streams with `stream_for`
     # expect(subscription).to have_stream_for(Room.find(42))
+    
+    data = {message_text: message.text, message_user_id: message.user_id, message_ticket_id: message.ticket_id}
+    # perform :reply, {message: data}
+    # expect(transmissions.last).to eq({'message': data})
+    perform :reply, data
+    expect(transmissions.last).to eq({'message': data})
   end
 end
