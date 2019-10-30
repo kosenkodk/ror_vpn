d8a8620fe24c022105ff140d88b15d290e4fe602
Dmitry K <kosenkodk@gmail.com>
2019-10-30 08:48:37 +0300

    Contact us with department tests

--- spec/features/contact_us_page_spec.rb
+++ spec/features/contact_us_page_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::ContactsController, type: :feature, js: true do
-  
+  let!(:department_billing) {create(:department, title: 'Billing')}
+
   before do
     visit '/contact_us'
   end
@@ -14,8 +15,8 @@ RSpec.describe Api::V1::ContactsController, type: :feature, js: true do
   end
 
   describe 'POST :create' do
-    let (:invalid_email) {'email@ex'}
-    let (:valid_email) {'email@ex.com'}
+    let(:invalid_email) {'email@ex'}
+    let(:valid_email) {'email@ex.com'}
 
     it 'clear error after resubmit form with valid email' do
       fill_in('email', with: invalid_email)
@@ -37,11 +38,14 @@ RSpec.describe Api::V1::ContactsController, type: :feature, js: true do
       it 'with correct email' do
         expect(page).to have_content('Send us a message and we will be in touch within 24 hours.')
         fill_in('email', with: valid_email)
+        
+        select(department_billing.title, from: 'departmentSelectBox')
+        # find("#departmentSelectBox").select(department_billing.title)
+        
         # fill_in('contact[email]', with: 'email@example.com')
         # fill_in('contact_message', with: 'message')
         # fill_in('contact_message_short', with: 'message short')
         fill_in('message_short', with: 'message short')
-        # select('Option', from: 'Alpha Department')
 
         click_button('Submit')
         success_message = I18n.t('pages.contact_us.success_message') # 'Your message has been created'
