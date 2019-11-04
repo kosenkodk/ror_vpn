208f6ab67720dceebfac392917c053d548bc929f
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-11-04 20:54:34 +0300

    Account. Change password with authorization.

--- app/controllers/api/v1/account_controller.rb
+++ app/controllers/api/v1/account_controller.rb
@@ -1,11 +1,10 @@
 class Api::V1::AccountController < ApplicationController
-  # before_action :find_user_by_id, only: [:update]
-  # before_action :authorize_access_request!
+  before_action :find_user, only: [:change_password]
+  before_action :authorize_access_request!
   KEYS = [:id, :password, :password_confirmation, :password_old].freeze
 
   def change_password
-    @user = User.find(params[:id])
-    if BCrypt::Password.new(@user.password_digest) == params[:password_old]
+    if is_old_pwd_ok
       @user.update!(password_params.except(:password_old))
       @user.clear_password_token!
       JWTSessions::Session.new(namespace: "user_#{@user.id}").flush_namespaced
@@ -16,12 +15,15 @@ class Api::V1::AccountController < ApplicationController
   end
 
   private
-  
+  def is_old_pwd_ok
+    BCrypt::Password.new(@user.password_digest) == params[:password_old]
+  end
+
   def password_params
     params.tap { |p| p.require(KEYS) }.permit(*KEYS)
   end
 
-  def find_user_by_id
+  def find_user
     @user = User.find(params[:id])
   end
 end
--- spec/controllers/api/v1/account_controller_spec.rb
+++ spec/controllers/api/v1/account_controller_spec.rb
@@ -3,19 +3,38 @@ require 'rails_helper'
 RSpec.describe Api::V1::AccountController, type: :controller do
   let(:user) { create(:user, password: 'password', password_confirmation: 'password') }
   let(:password_new) { 'newpassword' }
+  let(:access_cookie) { @tokens[:access] }
+  let(:csrf_token) { @tokens[:csrf] }
+ 
   describe 'change password' do
     context 'success' do
+      before do
+        # JWTSessions.access_exp_time = 3600
+        payload = { user_id: user.id }
+        session = JWTSessions::Session.new(payload: payload, 
+          refresh_by_access_allowed: true, 
+          namespace: "user_#{user.id}"
+        )
+        @tokens = session.login
+      end
       it 'with old, new and confirm passwords' do
+        request.cookies[JWTSessions.access_cookie] = access_cookie
+        request.headers[JWTSessions.csrf_header] = csrf_token
+
         patch :change_password, params: {id: user.id, password_old: 'password', password: password_new, password_confirmation: password_new}
         expect(response_json.values).to eq([I18n.t('pages.account.change_password.success')])
         expect(response_json.keys).to eq(['notice'])
         expect(response).to have_http_status(:success)
-        # expect(response_json.keys).to eq(['csrf'])
-        # expect(response.cookies[JWTSessions.access_cookie]).to be_present
+        expect(response.content_type).to eq('application/json; charset=utf-8')
       end
     end
     context 'failure' do
-      it 'without access token'
+      it 'without access token' do
+        patch :change_password, params: {id: user.id, password_old: 'password', password: password_new, password_confirmation: password_new}
+        expect(response_json.values).to eq(['Unauthorized'])
+        expect(response_json.keys).to eq(['error'])
+        expect(response).to have_http_status(:unauthorized)
+      end
     end
   end
 end
