560694dcad8a172862e7b1bb78beb9183126379d
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-11-04 20:22:12 +0300

    change password success context

--- spec/controllers/api/v1/account_controller_spec.rb
+++ spec/controllers/api/v1/account_controller_spec.rb
@@ -1,13 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::AccountController, type: :controller do
-  let(:user) { create(:user, password: 'password') }
+  let(:user) { create(:user, password: 'password', password_confirmation: 'password') }
   let(:password_new) { 'newpassword' }
   describe 'change password' do
     context 'success' do
       it 'with old, new and confirm passwords' do
-        patch :change_password, params: {password_old: 'password', password_new: password_new, password_confirmation: password_new}
+        patch :change_password, params: {id: user.id, password_old: 'password', password: password_new, password_confirmation: password_new}
+        expect(response_json.values).to eq([I18n.t('pages.account.change_password.success')])
+        expect(response_json.keys).to eq(['notice'])
         expect(response).to have_http_status(:success)
+        # expect(response_json.keys).to eq(['csrf'])
+        # expect(response.cookies[JWTSessions.access_cookie]).to be_present
       end
     end
     context 'failure' do
