0cc7e80be2cc6f37225b289e21d2a296eeb85804
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-11-06 18:26:22 +0300

    Destroy user with all associated data

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { create(:user) }
+  let!(:ticket) { create(:ticket, user: user) }
+  let!(:message) { create(:message, user: user, ticket: ticket) }
+
+  it 'destroy user' do
+    ticket.messages << message
+    expect(Ticket.where(user_id: user.id).count).to eq 1
+    expect(Ticket.where(user_id: user.id).first.messages.count).to eq 1
+    user_id = user.id
+    user.destroy
+    expect(User.where(id: user_id).count).to eq 0
+    expect(Ticket.where(user_id: user_id).count).to eq 0
+    expect(Message.where(user_id: user_id, ticket_id: ticket.id).count).to eq 0
+  end  
 end
