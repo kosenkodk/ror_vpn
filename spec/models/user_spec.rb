a7861c5e0eac6e020b9d86eca5b0591494c2cfbf
Dmitry K <kosenkodk@gmail.com>
2019-11-06 18:59:56 +0300

    User’s ticket with message and attachment

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,13 +6,15 @@ RSpec.describe User, type: :model do
   let!(:message) { create(:message, user: user, ticket: ticket) }
 
   it 'destroy user' do
+    message.attachment.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'logo.png')), filename: 'logo.png')
     ticket.messages << message
     expect(Ticket.where(user_id: user.id).count).to eq 1
     expect(Ticket.where(user_id: user.id).first.messages.count).to eq 1
+    expect(Message.where(user_id: user.id, ticket_id: ticket.id).count).to eq 1
     user_id = user.id
     user.destroy
     expect(User.where(id: user_id).count).to eq 0
     expect(Ticket.where(user_id: user_id).count).to eq 0
     expect(Message.where(user_id: user_id, ticket_id: ticket.id).count).to eq 0
-  end  
+  end
 end
