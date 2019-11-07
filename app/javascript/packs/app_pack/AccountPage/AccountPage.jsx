ee3d0f29bf95e33013d481739064677f1aff98e5
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-11-07 23:33:38 +0300

    Account page refactoring. Delete account button with popup.

--- app/javascript/packs/app_pack/AccountPage/AccountPage.jsx
+++ app/javascript/packs/app_pack/AccountPage/AccountPage.jsx
@@ -1,22 +1,80 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { ChangePassword, ChangeEmail } from './';
+import { ChangePassword, ChangePasswordForm, ChangeEmail, ChangeEmailForm, ModalPopup, DeleteForm } from './';
+import { accountActions } from '../_actions';
+import { FormDataAsJsonFromEvent } from '../_helpers';
+import { I18n } from 'helpers';
 
 class AccountPage extends React.Component {
   constructor(props) {
     super(props);
+    this.onAccountDelete = this.onAccountDelete.bind(this);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+  }
+
+  onChangePassword(e) {
+    e.preventDefault();
+    const data = FormDataAsJsonFromEvent(e);
+    this.props.dispatch(accountActions.changePassword(data));
+  }
+
+  onChangeEmail(e) {
+    e.preventDefault();
+    const data = FormDataAsJsonFromEvent(e);
+    this.props.dispatch(accountActions.changeEmail(data));
+  }
+
+  onAccountDelete(e) {
+    e.preventDefault();
+    // this.props.dispatch(accountActions.deleteAccount());
   }
 
   render() {
-    const { loggingIn } = this.props;
+    const { loggingIn, user } = this.props;
     return (
       <div className="col shadow-vega bg-vega p-4 mb-5"> {/* d-flex justify-content-lg-center"> */}
         <div className="container-section">
           <h2 className="pb-4">Account</h2>
-          <ChangeEmail idModal='changeEmail' />
-          <ChangePassword idModal='changePassword' />
+
+          {/* <ChangeEmail idModal='changeEmail' /> */}
+          <div className="mb-5">
+            <h4 id="email">Email</h4>
+            <div className="row">
+              <div className="col-sm-4 align-self-center">
+                <label className="col-form-label">Login email address: {user && user.email}</label>
+              </div>
+              <div className="col-sm-8">
+                <ModalPopup id='changeEmailModal' isForm={true} title='Change login email' btnText={I18n.t('pages.account.change_email.button')}>
+                  <ChangeEmailForm onFormSubmit={this.onChangeEmail} />
+                </ModalPopup>
+              </div>
+            </div>
+          </div>
+
+          {/* <ChangePassword idModal='changePassword' /> */}
+          <div className="mb-5">
+            <h4 id="email">Passwords</h4>
+            <div className="row">
+              <div className="col-sm-4 align-self-center">
+                <label className="col-form-label">Login password</label>
+              </div>
+              <div className="col-sm-8">
+                <ModalPopup id='changePasswordModal' isForm={true} title='Change login password' btnText={I18n.t('pages.account.change_password.button')}>
+                  <ChangePasswordForm onFormSubmit={this.onChangePassword} />
+                </ModalPopup>
+              </div>
+            </div>
+          </div>
+
           <h4 id="delete">Delete Account</h4>
-          <button className="btn btn-outline-pink active mb-5">Delete your account</button>
+          {/* <button type="button" className="btn btn-outline-pink active" onClick={this.onAccountDelete}>
+            Delete your account
+          </button> */}
+          <ModalPopup onSave={this.onAccountDelete} id='deleteAccountModal' title='Delete you account' btnText='Delete your account' btnCloseText={I18n.t('buttons.no')} btnSaveText={I18n.t('buttons.yes')}>
+            {/* <DeleteForm onFormSubmit={this.onAccountDelete} /> */}
+            Are you sure ?
+          </ModalPopup>
         </div>
       </div>
     );
@@ -28,9 +86,9 @@ AccountPage.defaultProps = {
 }
 
 function mapStateToProps(state) {
-  const { loggingIn } = state.authentication;
+  const { loggingIn, user } = state.authentication;
   return {
-    loggingIn
+    loggingIn, user
   };
 }
 
--- app/javascript/packs/app_pack/AccountPage/index.js
+++ app/javascript/packs/app_pack/AccountPage/index.js
@@ -1,3 +1,7 @@
 export * from './AccountPage';
 export * from './ChangePassword';
+export * from './ChangePasswordForm';
 export * from './ChangeEmail';
+export * from './ChangeEmailForm';
+export * from './DeleteForm';
+export * from './ModalPopup';
