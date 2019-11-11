4dde26a4101a981aa98faacee61c3574f579cdbc
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-11-11 21:46:09 +0300

    fix sign up

--- spec/controllers/api/v1/signup_controller_spec.rb
+++ spec/controllers/api/v1/signup_controller_spec.rb
@@ -1,27 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::SignupController, type: :controller do
-  describe 'POST #create' do
-    let(:tariff_plan) { create(:tariff_plan) }
-    let(:payment_method) { create(:payment_method) }
+  describe 'Sign up (POST #create)' do
+    let!(:tariff_plan) { create(:tariff_plan) }
+    let!(:payment_method) { create(:payment_method) }
     let(:email) { 'test@email.com' }
-    let(:user_params) { { email: email, password: 'password', password_confirmation: 'password', tariff_plan_id: tariff_plan.id, payment_method_id: payment_method.id } }
+    let(:user_params) { { email: email, password: 'password', password_confirmation: 'password' } }
+    let(:user_params_valid) { { email: email, password: 'password', password_confirmation: 'password', tariff_plan_id: tariff_plan.id, payment_method_id: payment_method.id } }
+    let(:user_params_invalid) { { email: email, password: 'password', password_confirmation: 'password', tariff_plan_id: '', payment_method_id: '' } }
+    let(:user_params_invalid2) { { email: email, password: 'password', password_confirmation: 'password', tariff_plan_id: nil, payment_method_id: nil } }
 
-    it 'returns http success' do
-      post :create, params: user_params
-      expect(response.status).to eq(200)
-      expect(response).to be_successful
-      expect(response_json.keys).to eq(['csrf'])
-      expect(response.cookies[JWTSessions.access_cookie]).to be_present
-      user = User.find_by(email: email)
-      expect(user.tariff_plan).to eq(tariff_plan)
-      expect(user.payment_method).to eq(payment_method)
+    context 'success' do
+      it 'without ids oftariff plan and payment method' do
+        post :create, params: user_params
+        # expect(response_json.values).to eq([''])
+
+        expect(response.status).to eq(200)
+        expect(response).to be_successful
+        expect(response_json.keys).to eq(['csrf'])
+        expect(response.cookies[JWTSessions.access_cookie]).to be_present
+      end
+
+      it 'with valid user params' do
+        post :create, params: user_params_valid
+        expect(response.status).to eq(200)
+        expect(response).to be_successful
+        expect(response_json.keys).to eq(['csrf'])
+        expect(response.cookies[JWTSessions.access_cookie]).to be_present
+        user = User.find_by(email: email)
+        expect(user.tariff_plan).to eq(tariff_plan)
+        expect(user.payment_method).to eq(payment_method)
+      end
+
+      it 'creates a new user' do
+        expect do
+          post :create, params: user_params_valid
+        end.to change(User, :count).by(1)
+      end
     end
 
-    it 'creates a new user' do
-      expect do
-        post :create, params: user_params
-      end.to change(User, :count).by(1)
+    xcontext 'failure' do
+      it 'with empty ids (payment method and tariff plan)' do
+        post :create, params: user_params_invalid
+        expect(response_json.keys).to eq(['error'])
+        expect(response_json.values).to eq(['Bad request'])
+      end
+
+      it 'with invalid params' do
+        post :create, params: user_params_invalid2
+        expect(response_json.keys).to eq(['error'])
+        expect(response_json.values).to eq(['Bad request'])
+      end
     end
   end
 end
