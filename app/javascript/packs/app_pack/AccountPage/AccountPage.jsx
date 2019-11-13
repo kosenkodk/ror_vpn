c9e053084628c351378462d99e611a1b682daf47
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-11-13 21:04:56 +0300

    section with white background

--- app/javascript/packs/app_pack/AccountPage/AccountPage.jsx
+++ app/javascript/packs/app_pack/AccountPage/AccountPage.jsx
@@ -39,43 +39,41 @@ class AccountPage extends React.Component {
   render() {
     const { loggingIn, user } = this.props;
     return (
-      <div className="col shadow-vega bg-lite p-4 mb-5"> {/* d-flex justify-content-lg-center"> */}
-        <div className="container-section">
-          <h2 className="pb-4">Account</h2>
+      <div className="container-section">
+        <h2 className="pb-4">Account</h2>
 
-          <div className="mb-5">
-            <h4 id="email">Email</h4>
-            <div className="row">
-              <div className="col-sm-4 align-self-center">
-                <label className="col-form-label">Login email address: {user && user.email}</label>
-              </div>
-              <div className="col-sm-8">
-                <ModalPopup onClose={this.clearModalAlerts} id='changeEmailModal' isForm={true} title='Change login email' btnText={I18n.t('pages.account.change_email.button')}>
-                  <ChangeEmailForm onModalClose={this.clearModalAlerts} onFormSubmit={this.onChangeEmail} />
-                </ModalPopup>
-              </div>
+        <div className="mb-5">
+          <h4 id="email">Email</h4>
+          <div className="row">
+            <div className="col-sm-4 align-self-center">
+              <label className="col-form-label">Login email address: {user && user.email}</label>
+            </div>
+            <div className="col-sm-8">
+              <ModalPopup onClose={this.clearModalAlerts} id='changeEmailModal' isForm={true} title='Change login email' btnText={I18n.t('pages.account.change_email.button')}>
+                <ChangeEmailForm onModalClose={this.clearModalAlerts} onFormSubmit={this.onChangeEmail} />
+              </ModalPopup>
             </div>
           </div>
-
-          <div className="mb-5">
-            <h4 id="password">Passwords</h4>
-            <div className="row">
-              <div className="col-sm-4 align-self-center">
-                <label className="col-form-label">Login password</label>
-              </div>
-              <div className="col-sm-8">
-                <ModalPopup onClose={this.clearModalAlerts} id='changePasswordModal' isForm={true} title='Change login password' btnText={I18n.t('pages.account.change_password.button')}>
-                  <ChangePasswordForm onModalClose={this.clearModalAlerts} onFormSubmit={this.onChangePassword} />
-                </ModalPopup>
-              </div>
-            </div>
-          </div>
-
-          <h4 id="delete">Delete Account</h4>
-          <ModalPopup onClose={this.clearModalAlerts} onBtnSave={this.onAccountDelete} id='deleteAccountModal' title='Delete you account' btnText={I18n.t('pages.account.delete.button')} btnCloseText={I18n.t('buttons.no')} btnSaveText={I18n.t('buttons.yes')}>
-            Are you sure ?
-          </ModalPopup>
         </div>
+
+        <div className="mb-5">
+          <h4 id="password">Passwords</h4>
+          <div className="row">
+            <div className="col-sm-4 align-self-center">
+              <label className="col-form-label">Login password</label>
+            </div>
+            <div className="col-sm-8">
+              <ModalPopup onClose={this.clearModalAlerts} id='changePasswordModal' isForm={true} title='Change login password' btnText={I18n.t('pages.account.change_password.button')}>
+                <ChangePasswordForm onModalClose={this.clearModalAlerts} onFormSubmit={this.onChangePassword} />
+              </ModalPopup>
+            </div>
+          </div>
+        </div>
+
+        <h4 id="delete">Delete Account</h4>
+        <ModalPopup onClose={this.clearModalAlerts} onBtnSave={this.onAccountDelete} id='deleteAccountModal' title='Delete you account' btnText={I18n.t('pages.account.delete.button')} btnCloseText={I18n.t('buttons.no')} btnSaveText={I18n.t('buttons.yes')}>
+          Are you sure ?
+          </ModalPopup>
       </div>
     );
   }
