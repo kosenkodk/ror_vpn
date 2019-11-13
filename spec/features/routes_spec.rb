c2e727e41f740041b785eb57edbdf66837b2fa45
Dmitry K <kosenkodk@gmail.com>
2019-11-13 11:09:26 +0300

    routes test contact us page

--- spec/features/routes_spec.rb
+++ spec/features/routes_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe AuthController, type: :feature, js: true do
-    let(:user) {create(:user)}
-    before { fsign_in_as(user) }
+  let(:user) { create(:user) }
 
-    it 'visit home page after signin' do
+  context 'after signin' do
+    before { fsign_in_as(user) }
+    
+    it 'visit home page' do
       visit('/')
       is_page_error false
     end
+
+    it 'visit contact us' do
+      visit('/contact_us')
+      expect(page).to have_content(I18n.t('pages.contact_us.subtitle'))
+    end
+  end
 end
