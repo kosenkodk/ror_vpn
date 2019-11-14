1b82688929f552962a71b56237f19c1fab232800
Dmitry K <kosenkodk@gmail.com>
2019-11-14 14:25:39 +0300

    white section with 85 % height

--- app/javascript/packs/app_pack/App/Layouts.jsx
+++ app/javascript/packs/app_pack/App/Layouts.jsx
@@ -16,7 +16,7 @@ const AdminLayout = (props) => (
         <div className='flex-grow-1'>
           <HeaderAdmin />
           <Alert />
-          <div className="col bg-lite p-4 mb-5">
+          <div className="col bg-lite p-4 mb-5" style={{ minHeight: '85vh' }}>
             {props.children}
           </div>
         </div>
