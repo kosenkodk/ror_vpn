feb0b45c0733746fbf53cc9faa20c1df09d3a5eb
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-11-15 16:07:41 +0300

    footer with current year

--- app/javascript/packs/app_pack/App/Layouts.jsx
+++ app/javascript/packs/app_pack/App/Layouts.jsx
@@ -7,16 +7,19 @@ import { Sidebar, Alert, Header } from '../_components';
 import { Header as HeaderAdmin } from '../_components/admin';
 
 const AdminLayout = (props) => (
-  <div id="admin_layout" className='container-fluid '>
+  <div id='admin_layout' className='container-fluid '>
     <div className='row'>
-      <div className='col-md-2 background-black'>
-        <Sidebar />
+      <div className='d-flex flex-column col-md-2 background-black'>
+        <div className='flex-grow-1'>
+          <Sidebar />
+        </div>
+        <div class='align-self-center mb-2'>© {new Date(Date.now()).getFullYear()} Vega Vpn</div>
       </div>
       <div style={{ minHeight: '100vh' }} className='col-md-10 background-pink d-flex flex-column align-items-stretch'>
         <div className='flex-grow-1'>
           <HeaderAdmin />
           <Alert />
-          <div className="col bg-lite pt-2 mb-5" style={{ minHeight: '85vh' }}>
+          <div className='col bg-lite pt-2 mb-5' style={{ minHeight: '85vh' }}>
             {props.children}
           </div>
         </div>
