ad8e428397b0e3e856c8cff4c7deb21a0b3baa7b
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-11-20 18:55:23 +0300

    security and trust section without bg image (separate 2 images at the left and at the right)

---
+++ app/javascript/packs/app_pack/_sections/SecurityAndTrustSeperateImages.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import SecurityImageSrc from 'images/bg_security_and_trust2'
+import SecurityCometsSrc from 'images/bg_security_and_trust2_comets'
+
+class SecurityAndTrust extends React.Component {
+  render() {
+    return (
+      <div id="security_and_trust" className="row card d-flex flex-column">
+        <div className="row no-gutters d-flex align-items-center">
+          <div className="col-md-4">
+            <img src={SecurityImageSrc} className="img-fluid" />
+          </div>
+          <div className="col-md-4">
+            <div className="card-body">
+              <h1 className="card-title">Built for security and trust</h1>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <img src={SecurityCometsSrc} className="img-fluid" />
+          </div>
+          <div className="col-md-4">
+            <div className="card-body d-none d-md-block"></div>
+          </div>
+          <div className="col-md-4">
+            <div className="card-body">
+              <h5 className="card-title">Private</h5>
+              <p className="card-text">Keep your real location and Unique IP address hidden, prevent Anyone from viewing your
+                Internet activity.
+              </p>
+              {/* <!-- <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p> --> */}
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card-body">
+              <h5 className="card-title">Secure</h5>
+              <p className="card-text">Use WiFi Hotspots Safe From cyber criminals, block incoming threats and DDoS Attacks With
+                Our Firewall.
+            </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SecurityAndTrust
