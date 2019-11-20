f92cc74b8e97803f750644ee0226efb743afb162
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-11-20 16:44:31 +0300

    app downloads section with svgs

--- app/javascript/packs/app_pack/_sections/AppDownloads.jsx
+++ app/javascript/packs/app_pack/_sections/AppDownloads.jsx
@@ -3,10 +3,10 @@ import { I18n } from 'helpers'
 import Vega from 'images/vega_vertical.png'
 import VegaForExtraSmallScreens from 'images/vega_horizontal.png'
 
-import Apple from 'images/downloads/Apple'
-import Android from 'images/downloads/Android'
-import WP from 'images/downloads/WP'
-import Linux from 'images/downloads/Linux'
+import Apple from 'images/downloads/svg/Apple'
+import Android from 'images/downloads/svg/Android'
+import WP from 'images/downloads/svg/WP'
+import Linux from 'images/downloads/svg/Linux'
 
 class AppDownloads extends React.Component {
   render() {
