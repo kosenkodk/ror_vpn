8f2bc10c4d63eadcb37818c79e1d0e587223914c
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-11-21 21:41:54 +0300

    Reviews section improve styles and fonts

--- app/javascript/packs/app_pack/_sections/Reviews.jsx
+++ app/javascript/packs/app_pack/_sections/Reviews.jsx
@@ -1,29 +1,32 @@
 import React from 'react'
-import { I18n } from 'helpers'
+// import { I18n } from 'helpers'
 import ReviewSummary from './ReviewSummary'
 import ReviewOwner from './ReviewOwner'
+// import imgSecuritySrc from 'images/compare/reviews'
 
 class Reviews extends React.Component {
   render() {
     return (
       <div className="row reviews card shadow-vega">
+        {/* <img src={imgSecuritySrc} className=" img-fluid" /> */}
+
         <div className="col-md-10 offset-md-1 text-center">
-          <div className="card-header">
+          <div className="card-header mt-4">
             Why the Media loves VPN
           </div>
           <div className="tab-content" id="pills-tabContent">
             <div className="tab-pane fade show active" id="pills-review1" role="tabpanel" aria-labelledby="pills-review1-tab">
-              1 <ReviewSummary />
+              <ReviewSummary no='1' />
             </div>
             <div className="tab-pane fade" id="pills-review2" role="tabpanel" aria-labelledby="pills-review2-tab">
-              2 <ReviewSummary />
+              <ReviewSummary no='2' />
             </div>
             <div className="tab-pane fade" id="pills-review3" role="tabpanel" aria-labelledby="pills-review3-tab">
-              3 <ReviewSummary />
+              <ReviewSummary no='3' />
             </div>
           </div>
           <div className="card-footer text-left">
-            <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
+            <ul className="nav nav-pills mt-3 mb-2" id="pills-tab" role="tablist">
               <li className="nav-item col-sm-6 col-md-4">
                 <div className="review-owner-container" id="pills-review1-tab" data-toggle="pill" href="#pills-review1" role="tab"
                   aria-controls="pills-review1" aria-selected="true">
