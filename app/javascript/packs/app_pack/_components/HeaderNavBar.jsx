54e3b587eed21a04007ff7985dc44b686d729505
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-11-24 14:11:59 +0300

    narrow navbar

--- app/javascript/packs/app_pack/_components/HeaderNavBar.jsx
+++ app/javascript/packs/app_pack/_components/HeaderNavBar.jsx
@@ -17,41 +17,43 @@ class HeaderNavBar extends React.Component {
     const { loggedIn } = this.props;
     return (
       <nav id="navbar" className="nav navbar navbar-expand-md navbar-dark fixed-top shadow-vega">
-        <NavHashLink smooth to={urls.home.path}>
-          <img src={logoImage} className='navbar-brand mr-auto ml-2' alt='Vega VPN'></img>
-        </NavHashLink>
+        <div className="container">
+          <NavHashLink smooth to={urls.home.path}>
+            <img src={logoImage} className='navbar-brand mr-auto ml-2' alt='Vega VPN'></img>
+          </NavHashLink>
 
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar6">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="navbar-collapse collapse justify-content-stretch" id="navbar6">
-          {
-            loggedIn ?
-              <ul className="navbar-nav ml-auto">
-                {[
-                  // urls.help, urls.user_dashboard, urls.user_account, urls.tickets,
-                  // urls.user_payment, urls.user_downloads, urls.user_invite_friend,
-                  urls.user_dashboard, urls.features, urls.pricing, urls.downloads, urls.contact_us, urls.help,
-                ].map((item, index) =>
-                  <li key={`nav-private${index}`} className="nav-item pr-1 pl-1">
-                    <NavHashLink smooth to={item.path} activeClassName="" className="nav-link pl-3 pr-3 text-left btn btn-outline-pink">{item.name}</NavHashLink>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar6">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="navbar-collapse collapse justify-content-stretch" id="navbar6">
+            {
+              loggedIn ?
+                <ul className="navbar-nav ml-auto">
+                  {[
+                    // urls.help, urls.user_dashboard, urls.user_account, urls.tickets,
+                    // urls.user_payment, urls.user_downloads, urls.user_invite_friend,
+                    urls.user_dashboard, urls.features, urls.pricing, urls.downloads, urls.contact_us, urls.help,
+                  ].map((item, index) =>
+                    <li key={`nav-private${index}`} className="nav-item pr-1 pl-1">
+                      <NavHashLink smooth to={item.path} activeClassName="" className="nav-link pl-3 pr-3 text-left btn btn-outline-pink">{item.name}</NavHashLink>
+                    </li>
+                  )}
+                  <li className="nav-item">
+                    <NavHashLink to={urls.signout.path} onClick={this.signOut} activeClassName="" className="nav-link pl-3 pr-3 text-left btn btn-outline-pink active">{urls.signout.name}</NavHashLink>
                   </li>
-                )}
-                <li className="nav-item">
-                  <NavHashLink to={urls.signout.path} onClick={this.signOut} activeClassName="" className="nav-link pl-3 pr-3 text-left btn btn-outline-pink active">{urls.signout.name}</NavHashLink>
-                </li>
-              </ul>
-              :
-              <ul className="navbar-nav ml-auto">
-                {[
-                  urls.features, urls.pricing, urls.downloads, urls.contact_us, urls.help, urls.signin, urls.signup
-                ].map((item, index) =>
-                  <li key={`nav-pubic${index}`} className="nav-item pr-1 pl-1">
-                    <NavHashLink smooth to={item.path} activeClassName="" className={`nav-link pl-3 pr-3 text-left btn btn-outline-pink ${item.isActive ? 'active' : ''}`}>{item.name}</NavHashLink>
-                  </li>
-                )}
-              </ul>
-          }
+                </ul>
+                :
+                <ul className="navbar-nav ml-auto">
+                  {[
+                    urls.features, urls.pricing, urls.downloads, urls.contact_us, urls.help, urls.signin, urls.signup
+                  ].map((item, index) =>
+                    <li key={`nav-pubic${index}`} className="nav-item pr-1 pl-1">
+                      <NavHashLink smooth to={item.path} activeClassName="" className={`nav-link pl-3 pr-3 text-left btn btn-outline-pink ${item.isActive ? 'active' : ''}`}>{item.name}</NavHashLink>
+                    </li>
+                  )}
+                </ul>
+            }
+          </div>
         </div>
       </nav>
     )
