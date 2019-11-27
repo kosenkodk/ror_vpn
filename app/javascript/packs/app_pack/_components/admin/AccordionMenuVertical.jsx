097a5df76f41435e4b75ca179d32ca95532620b4
Dmitry K <kosenkodk@gmail.com>
2019-11-27 22:21:42 +0300

    accordion static

--- app/javascript/packs/app_pack/_components/admin/AccordionMenuVertical.jsx
+++ app/javascript/packs/app_pack/_components/admin/AccordionMenuVertical.jsx
@@ -7,13 +7,13 @@ class AccordionMenuVertical extends React.Component {
 
   render() {
     const { items } = this.props;
-    const content2 =
+    const content_static =
       <div class="accordion" id="accordionExample" >
         <div class="card">
           <div class="card-header" id="headingOne">
             <h5 class="mb-0">
               <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                Collapsible Group Item #1
+                Item #1
             </button>
             </h5>
           </div>
@@ -30,7 +30,7 @@ class AccordionMenuVertical extends React.Component {
             <div class="card-header" id="headingTwo">
               <h5 class="mb-0">
                 <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                  Collapsible Group Item #2
+                  Item #2
             </button>
               </h5>
             </div>
@@ -46,14 +46,15 @@ class AccordionMenuVertical extends React.Component {
           <div class="card-header" id="headingThree">
             <h5 class="mb-0">
               <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                Collapsible Group Item #3
+                Item #3
             </button>
             </h5>
           </div>
           <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
             <div class="card-body">
               Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-          </div>
+              {/* <ul class="submenu border"><li><a class="text-light" href="/user/downloads#configs">OpenVPN configs</a></li><li><a class="text-light" href="/user/downloads/#apps">Applications</a></li></ul> */}
+            </div>
           </div>
         </div>
       </div>
@@ -63,15 +64,15 @@ class AccordionMenuVertical extends React.Component {
         <div key={item.path} class="card">
           {item.urls ?
             <React.Fragment>
-              <div class="card-header" id={`heading${index}`}>
-                <h5 class="mb-0">
-                  <button class="btn btn-link" type="button" data-toggle="collapse" data-target={`#collapse${index}`} aria-expanded="true" aria-controls={`collapse${index}`}>
-                    {item.name}
-                  </button>
-                </h5>
+              <div class="card-header" id={`heading${accordionId}${index}`}>
+                {/* <h5 class="mb-0"> */}
+                <button class="btn btn-link" type="button" data-toggle="collapse" data-target={`#collapse${accordionId}${index}`} aria-expanded="true" aria-controls={`collapse${accordionId}${index}`}>
+                  {item.name}
+                </button>
+                {/* </h5> */}
               </div>
 
-              <div id={`collapse${index}`} class="collapse" aria-labelledby={`heading${index}`} data-parent={`#${accordionId}`}>
+              <div id={`collapse${accordionId}${index}`} class="collapse" aria-labelledby={`heading${accordionId}${index}`} data-parent={`#${accordionId}`}>
                 <div class="card-body border">
                   <ul className="submenu border">
                     {item.urls && Object.values(item.urls).map(subItem =>
@@ -99,7 +100,7 @@ class AccordionMenuVertical extends React.Component {
    </div>
     return (
       <React.Fragment>
-        {content2}
+        {content_static}
         {content}
       </React.Fragment>
     );
