2a43927169ee6f03cf571bf35db90c27fc1aba4d
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-12-06 15:28:08 +0300

    account page - single section (recovery and notification) instead of email and passwords sections

--- app/javascript/packs/app_pack/AccountPage/AccountPage.jsx
+++ app/javascript/packs/app_pack/AccountPage/AccountPage.jsx
@@ -39,43 +39,85 @@ class AccountPage extends React.Component {
   render() {
     const { loggingIn, user } = this.props;
     return (
-      <div className="container-section">
-        {/* <h2 className="pb-4">Account</h2> */}
-        <div className="mb-5">
-          <h1 id="email">Email</h1>
-          <div className="row">
-            <div className="col-sm-4 align-self-center">
-              <label className="col-form-label">Login email address: {user && user.email}</label>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col">
+            {/* <h2 className="pb-4">Account</h2> */}
+            {/* <div className="mb-5">
+              <h1 id="email">Email</h1>
+              <div className="row">
+                <div className="col-sm-4 align-self-center">
+                  <label className="col-form-label">Login email address: {user && user.email}</label>
+                </div>
+                <div className="col-sm-8">
+                  <ModalPopup onClose={this.clearModalAlerts} id='changeEmailModal' isForm={true} title='Change login email' btnText={I18n.t('pages.account.change_email.button')}>
+                    <ChangeEmailForm onModalClose={this.clearModalAlerts} onFormSubmit={this.onChangeEmail} />
+                  </ModalPopup>
+                </div>
+              </div>
+            </div> */}
+
+            <div className="mb-5">
+              <h1 id="password">Recovery & notification</h1>
+
+              <div class="border-left-pink col-sm-8">
+                {/* <h5 id="caveat-with-anchors">WARNING: DELETION IS PERMANENT</h5> */}
+                <p>
+                  The selected method can be used to recover an account in the event your forget your password and to be notified about missed emails.
+                </p>
+              </div>
+
+              <div className="row">
+                <div className="col-sm-3 align-self-center">
+                  <label className="col-form-label">Login email address:</label>
+                </div>
+                <div className="col-sm-4">
+                  <input type="string" name="email" className="col-sm-6 form-control" id="email" value={user && user.email} onChange={this.handleChange} placeholder='Email' />
+                </div>
+                <div className="col">
+                  <ModalPopup onClose={this.clearModalAlerts} id='changeEmailModal' isForm={true} title='Change login email' btnText={I18n.t('buttons.edit')}>
+                    <ChangeEmailForm onModalClose={this.clearModalAlerts} onFormSubmit={this.onChangeEmail} />
+                  </ModalPopup>
+                </div>
+
+              </div>
+
+              <div className="row">
+                <div className="col-sm-3 align-self-center">
+                  <label className="col-form-label">Allow password reset</label>
+                </div>
+
+                <div class="col">
+                  <div class="custom-control custom-switch">
+                    <input type="checkbox" class="custom-control-input" id="customSwitch1" />
+                    <label class="custom-control-label" for="customSwitch1"></label>
+
+                    {/* <div class="custom-control custom-switch">
+                      <input type="checkbox" class="custom-control-input" disabled id="customSwitch2" />
+                      <label class="custom-control-label" for="customSwitch2">Disabled switch element</label>
+                    </div> */}
+                  </div>
+                </div>
+
+                <div className="col-sm-8">
+                  <ModalPopup onClose={this.clearModalAlerts} id='changePasswordModal' isForm={true} title='Change login password' btnText={I18n.t('pages.account.change_password.button')}>
+                    <ChangePasswordForm onModalClose={this.clearModalAlerts} onFormSubmit={this.onChangePassword} />
+                  </ModalPopup>
+                </div>
+              </div>
             </div>
-            <div className="col-sm-8">
-              <ModalPopup onClose={this.clearModalAlerts} id='changeEmailModal' isForm={true} title='Change login email' btnText={I18n.t('pages.account.change_email.button')}>
-                <ChangeEmailForm onModalClose={this.clearModalAlerts} onFormSubmit={this.onChangeEmail} />
-              </ModalPopup>
-            </div>
+
+            <h1 id="delete">Delete Account</h1>
+            <ModalPopup onClose={this.clearModalAlerts} onBtnSave={this.onAccountDelete} id='deleteAccountModal' title='Delete you account' btnText={I18n.t('pages.account.delete.button')} btnCloseText={I18n.t('buttons.cancel')} btnSaveText={I18n.t('buttons.delete')}>
+              <div class="bd-callout bd-callout-warning">
+                <h5 id="caveat-with-anchors">WARNING: DELETION IS PERMANENT</h5>
+                <p>If you wish to delete this account in order to combine it with another one, do NOT delete it.
+                  {/* <a>Learn more</a> */}
+                </p>
+              </div>
+            </ModalPopup>
           </div>
         </div>
-
-        <div className="mb-5">
-          <h1 id="password">Passwords</h1>
-          <div className="row">
-            <div className="col-sm-4 align-self-center">
-              <label className="col-form-label">Login password</label>
-            </div>
-            <div className="col-sm-8">
-              <ModalPopup onClose={this.clearModalAlerts} id='changePasswordModal' isForm={true} title='Change login password' btnText={I18n.t('pages.account.change_password.button')}>
-                <ChangePasswordForm onModalClose={this.clearModalAlerts} onFormSubmit={this.onChangePassword} />
-              </ModalPopup>
-            </div>
-          </div>
-        </div>
-
-        <h1 id="delete">Delete Account</h1>
-        <ModalPopup onClose={this.clearModalAlerts} onBtnSave={this.onAccountDelete} id='deleteAccountModal' title='Delete you account' btnText={I18n.t('pages.account.delete.button')} btnCloseText={I18n.t('buttons.cancel')} btnSaveText={I18n.t('buttons.delete')}>
-          <div class="bd-callout bd-callout-warning">
-            <h5 id="caveat-with-anchors">WARNING: DELETION IS PERMANENT</h5>
-            <p>If you wish to delete this account in order to combine it with another one, do NOT delete it. <a>Learn more</a></p>
-          </div>
-        </ModalPopup>
       </div>
     );
   }
