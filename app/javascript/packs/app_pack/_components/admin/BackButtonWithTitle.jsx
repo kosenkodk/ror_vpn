bcde578fd0380e320489b008fdae04ac2b9c0b9a
Dmitry K <kosenkodk@gmail.com>
2019-12-16 19:41:22 +0300

    bottom line after back button with title

--- app/javascript/packs/app_pack/_components/admin/BackButtonWithTitle.jsx
+++ app/javascript/packs/app_pack/_components/admin/BackButtonWithTitle.jsx
@@ -7,17 +7,20 @@ class BackButtonWithTitle extends React.Component {
   render() {
     const { title, url, children } = this.props;
     return (
-      <div className="btn-back-with-title d-flex">
-        <div className="align-self-center mb-1">
-          <Link to={url}>
-            <img src={icArrowLeftSrc} className="img-fluid arrow" />
-          </Link>
+      <React.Fragment>
+        <div className="btn-back-with-title d-flex">
+          <div className="align-self-center mb-1">
+            <Link to={url}>
+              <img src={icArrowLeftSrc} className="img-fluid arrow" />
+            </Link>
+          </div>
+          <div className="align-self-center">
+            {title && <h4 className="mt-2 font-weight-bold">{title}</h4>}
+            {children && children}
+          </div>
         </div>
-        <div className="align-self-center">
-          {title && <h4 className="mt-2 font-weight-bold">{title}</h4>}
-          {children && children}
-        </div>
-      </div>
+        <div class="flex-row mt-3 mb-3"><div class="border border-gray"></div></div>
+      </React.Fragment>
     );
   }
 }
