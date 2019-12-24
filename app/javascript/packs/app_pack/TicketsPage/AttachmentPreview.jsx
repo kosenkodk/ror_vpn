7808d78048ee81dc0d992eb8a14bdb25db47eb47
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-12-24 16:35:04 +0300

    attachment preview for ticket's form

---
+++ app/javascript/packs/app_pack/TicketsPage/AttachmentPreview.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class AttachmentPreview extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        {(this.props.items && this.props.items.length > 0) &&
+          <div className="form-group row multi-preview no-gutters">
+            {(this.props.items || []).map((url, index) => (
+              <div className="col-sm-6 col-md-4 p-1" key={`attachment-preview-${index}`} >
+                <img className="img-fluid img-thumbnail" src={url} alt="..." />
+              </div>
+            ))}
+          </div>
+        }
+      </React.Fragment>
+    );
+  }
+}
+
+export default AttachmentPreview;
