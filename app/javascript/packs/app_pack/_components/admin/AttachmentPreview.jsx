83e3ded20be9318af56b0b95711d380e135b8485
Dmitry K <kosenkodk@gmail.com>
2019-12-24 22:23:21 +0300

    attachment preview (pdf and images)

--- app/javascript/packs/app_pack/_components/admin/AttachmentPreview.jsx
+++ app/javascript/packs/app_pack/_components/admin/AttachmentPreview.jsx
@@ -6,9 +6,15 @@ class AttachmentPreview extends React.Component {
       <React.Fragment>
         {(this.props.items && this.props.items.length > 0) &&
           <div className="form-group row multi-preview no-gutters">
-            {(this.props.items || []).map((url, index) => (
+            {(this.props.items || []).map((item, index) => (
               <div className="col-sm-6 col-md-4 p-1" key={`attachment-preview-${index}`} >
-                <img className="img-fluid img-thumbnail" src={url} alt="..." />
+                {item.file.type === 'application/pdf' ?
+                  <a href={item.url}>{item.file.name}
+                    {/* {`${item.file.size > 0 ? item.file.size / 1000 + ' Kb' : ''}`} */}
+                  </a>
+                  :
+                  <img className="img-fluid img-thumbnail" src={item.url} alt={item.file.name} />
+                }
               </div>
             ))}
           </div>
