8ae0cce3083e64509d529eed060a869a83d14f40
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-12-25 12:53:32 +0300

    clear unused code for prev commit

--- app/javascript/packs/app_pack/_components/admin/AttachmentPreview.jsx
+++ app/javascript/packs/app_pack/_components/admin/AttachmentPreview.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import icPdf from 'images/icons/ic_pdf3'
 
 class AttachmentPreview extends React.Component {
   render() {
@@ -9,7 +10,8 @@ class AttachmentPreview extends React.Component {
             {(this.props.items || []).map((item, index) => (
               <div className="col-sm-6 col-md-4 p-1" key={`attachment-preview-${index}`} >
                 {item.file.type === 'application/pdf' ?
-                  <a href={item.url}>{item.file.name}
+                  <a href={item.url}>
+                    <img className="img-fluid img-thumbnail cover" src={icPdf} alt={item.file.name} />
                     {/* {`${item.file.size > 0 ? item.file.size / 1000 + ' Kb' : ''}`} */}
                   </a>
                   :
