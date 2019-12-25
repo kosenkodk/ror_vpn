f62ce0bd8cc44305af34716fd198a2e4ce6bc29c
Dmitry K <kosenkodk@gmail.com>
2019-12-25 22:11:12 +0300

    attachments - set columns for small, medium and large screens

--- app/javascript/packs/app_pack/_components/admin/AttachmentPreviewCard.jsx
+++ app/javascript/packs/app_pack/_components/admin/AttachmentPreviewCard.jsx
@@ -5,17 +5,18 @@ class AttachmentPreviewCard extends React.Component {
   render() {
     return (
       <React.Fragment>
-        {(this.props.items && this.props.items.length > 0) && <div className="card-group">
-          {(this.props.items || []).map((item, index) => (
-            <div className="col-sm-4 col-md-3" key={`attachment-preview-${index}`} >
-              {(item.file.type === 'application/pdf') ?
-                <AttachmentPdf name={item.file.name} url={item.url} index={index} />
-                :
-                <AttachmentImage name={item.file.name} url={item.url} index={index} />
-              }
-            </div>
-          ))}
-        </div>
+        {(this.props.items && this.props.items.length > 0) &&
+          <div className="card-group row">
+            {(this.props.items || []).map((item, index) => (
+              <div className="col-sm-4 col-md-3 col-lg-2" key={`attachment-preview-${index}`} >
+                {(item.file.type === 'application/pdf') ?
+                  <AttachmentPdf name={item.file.name} url={item.url} index={index} />
+                  :
+                  <AttachmentImage name={item.file.name} url={item.url} index={index} />
+                }
+              </div>
+            ))}
+          </div>
         }
       </React.Fragment>
     );
