a2ac66f871e43760be4daa6dd876cb814f612e7c
Dmitry K <kosenkodk@gmail.com>
2019-12-26 19:07:47 +0300

    file name of attachment image with the same height

--- app/javascript/packs/app_pack/_components/admin/AttachmentImage.jsx
+++ app/javascript/packs/app_pack/_components/admin/AttachmentImage.jsx
@@ -7,24 +7,35 @@ class AttachmentImage extends React.Component {
     const item = { 'name': name, 'url': url };
     return (
       <React.Fragment>
+        <div class="row row-flex">
+          <div class="col">
+            <div class="content">
+              <ModalPopup id={`modalForAttachmentImage${index}`} aImgClasses='card-img-top embed-responsive-item cover' aUrl={item.url} aImgSrc={item.url} aTitle={item.name} title={item.name} isShowFooter={false} >
+                <img src={item.url} className="card-img-top embed-responsive-item cover" alt={item.name} />
+              </ModalPopup>
+              <p className="word-wrap p-2"><small>{item.name}</small></p>
+            </div>
+          </div>
+        </div>
+
         {/* <figure class="figure row">
           <img src={item.url} class="col figure-img img-fluid rounded" alt="..." />
           <figcaption class="col figure-caption text-right text-wrap">{item.name}</figcaption>
         </figure> */}
-        <div className="card">
+
+        {/* <div className="card">
           <div className="embed-responsive embed-responsive-16by9">
             <ModalPopup id={`modalForAttachmentImage${index}`} aImgClasses='card-img-top embed-responsive-item cover' aUrl={item.url} aImgSrc={item.url} aTitle={item.name} title={item.name} isShowFooter={false} >
               <img src={item.url} className="card-img-top embed-responsive-item cover" alt={item.name} />
             </ModalPopup>
           </div>
           <div className="card-footer">
-            <div className="embed-responsive embed-responsive-16by9">
-              <small className="text-muted embed-responsive-item mx-auto">
-                {item.name}
-              </small>
+            <div className="d-flex align-content-center flex-wrap">
+              {item.name}
             </div>
-          </div>
-          {/* <div className="card-body">
+          </div> */}
+
+        {/* <div className="card-body">
           <h5 className="card-title">{item.name}</h5>
           <p className="card-text">
             <a className="" href={item.url}>{item.name}</a>
@@ -35,8 +46,9 @@ class AttachmentImage extends React.Component {
             {item.name}
           </small>
         </div> */}
-        </div>
-      </React.Fragment>
+
+        {/* </div > */}
+      </React.Fragment >
     );
   }
 }
