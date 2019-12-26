489f187cae454927ace58fc46f6485b2eceef49d
Dmitry K <kosenkodk@gmail.com>
2019-12-26 21:33:16 +0300

    use https instead of http

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
@@ -78,6 +78,8 @@ Rails.application.configure do
 
   config.host = 'vega.isit.su'
   Rails.application.routes.default_url_options[:host] = config.host
+  # Rails.application.routes.default_url_options[:protocol] = 'https'
+
   config.action_mailer.default_url_options = { host: config.host }
   config.action_mailer.default_options = { from: "VegaVPN <no-reply@#{config.host}>" }
   # config.action_mailer.asset_host = "#{Rails.application.config.host}/packs/media/images/"
