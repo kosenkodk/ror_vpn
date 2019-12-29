0b76ae100a8cbd7ab11490198c24dacca98bc33b
Dmitry K <kosenkodk@gmail.com>
2019-12-29 22:58:51 +0300

    remove commented code

--- spec/support/response_helper.rb
+++ spec/support/response_helper.rb
@@ -2,11 +2,4 @@ module ResponseHelper
   def response_json
     JSON.parse(response.body) rescue {}
   end
-
-  def change_email_check_error
-    expect(response_json.keys).to eq(['error'])
-    expect(response_json['error']).to eq(error)
-    expect(response).to have_http_status(422)
-    expect(response.content_type).to eq('application/json; charset=utf-8')
-  end
 end
