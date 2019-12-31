d720d22cc7aa1dc86d8c35a929d239f8ba98da3b
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-12-31 13:21:27 +0300

    add model test

--- spec/models/cancel_reason_spec.rb
+++ spec/models/cancel_reason_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe CancelReason, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:title) { 'title' }
+  let(:text) { 'text' }
+  let(:order) { 10 }
+  it do
+    item = create(:cancel_reason, title:title, text: text, order: order)
+    expect(item.title).to eq(title)
+    expect(item.text).to eq(text)
+    expect(item.order).to eq(order)
+  end
 end
