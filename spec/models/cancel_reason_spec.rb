1e7c0e1b07243529420addff8e4ef1ce8abcc573
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-12-31 13:28:17 +0300

    add has_many association to cancel reason model

--- spec/models/cancel_reason_spec.rb
+++ spec/models/cancel_reason_spec.rb
@@ -4,10 +4,19 @@ RSpec.describe CancelReason, type: :model do
   let(:title) { 'title' }
   let(:text) { 'text' }
   let(:order) { 10 }
+  let(:user) { create(:user) }
+  let(:item) { create(:cancel_reason, title: title, text: text, order: order) }
+  
   it do
-    item = create(:cancel_reason, title:title, text: text, order: order)
     expect(item.title).to eq(title)
     expect(item.text).to eq(text)
     expect(item.order).to eq(order)
+  end
+  
+  it 'add cancel reason to user' do
+    user.cancel_reason = item
+    user.save
+    expect(user.cancel_reason).to eq(item)
+    expect(item.users.last).to eq(user)
   end
 end
