9eafcb0539bad405cc8742dcdbc3c31ab88c3da7
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2020-01-06 17:49:40 +0300

    sometimes back history button is not working properly

--- app/javascript/packs/app_pack/_helpers/history.js
+++ app/javascript/packs/app_pack/_helpers/history.js
@@ -1,3 +1,10 @@
-import { createBrowserHistory } from 'history';
+// import { createBrowserHistory } from 'history';
+// export const history = createBrowserHistory();
+// export const history = createBrowserHistory(
+//   {
+//     forceRefresh: false, // Set true to force full page refreshes
+//   }
+// );
 
+import createBrowserHistory from 'history/createBrowserHistory'
 export const history = createBrowserHistory();
