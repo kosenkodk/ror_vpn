23871232b604a6ae9d869f7913e2443e80090ebf
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2020-01-09 21:15:05 +0300

    fix tests of account page with autohidden alerts

--- spec/support/feature_helper.rb
+++ spec/support/feature_helper.rb
@@ -9,10 +9,10 @@ module FeatureHelper
   def check_clear_alerts_on_modal_close(button_open, button_save, button_close)
     click_on(button_open)
     click_on(button_save)
-    expect(page).to have_selector('.alert')
+    expect(page).to have_selector('.alert', visible: :all)
     click_on(button_close)
     click_on(button_open)
-    expect(page).not_to have_selector('.alert')
+    expect(page).not_to have_selector('.alert', visible: :all)
   end
 
   def is_page_error is_page_error
@@ -50,5 +50,21 @@ module FeatureHelper
     select(cancel_reason.title, from: id_of_select_box)
     # find('#'+id_of_select_box).select(cancel_reason.title)
     expect(find('#'+id_of_select_box).value.to_i).to eq(cancel_reason.id)
+  end
+
+  def alert_have_text text
+    expect(page).to have_css('.alert', visible: :hidden, text: text)
+
+    # it works for permanent alerts/flash messages
+    # expect(find('.alert')).to have_text(text)
+
+    # if alert is hidden
+    expect(page).to have_css('.alert', visible: :hidden, text: text)
+    # # to wait for alert to be removed from the dom
+    # expect(page).not_to have_css('.alert', visible: :hidden, text: text)
+    
+    # # workaround for capybara < 2.11.0 
+    # alert = page.find('.alert', visible: :all, text: text)
+    # expect(alert).to match_css('.alert', visible: :hidden)
   end
 end
