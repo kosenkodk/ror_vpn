f9d9af76e4b2fc70965916dbc28f6163f15a59a8
Dmitry K <kosenkodk@gmail.com>
2020-01-12 01:42:06 +0300

    fix ticket's attachment modal popups

--- app/javascript/packs/app_pack/_components/admin/AttachmentPdf.jsx
+++ app/javascript/packs/app_pack/_components/admin/AttachmentPdf.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import icPdf from 'images/icons/ic_pdf3';
-import { ModalPopup } from '../../_components';
+import { ModalPopupForm } from '../../_components';
 
 class AttachmentPdf extends React.Component {
   constructor(props) {
@@ -24,12 +24,12 @@ class AttachmentPdf extends React.Component {
         <div class="col">
           <div class="h-100" onClick={this.isWillLoad}>
             <div className="embed-responsive embed-responsive-16by9">
-              <ModalPopup id={`modalForAttachmentPdf${index}`} aImgClasses='card-img-top embed-responsive-item cover' aUrl={item.url} aImgSrc={icPdf} aTitle={item.name} title={item.name} isShowFooter={false} >
+              <ModalPopupForm id={`modalForAttachmentPdf${index}`} aImgClasses='card-img-top embed-responsive-item cover' aUrl={item.url} aImgSrc={icPdf} aTitle={item.name} title={item.name} isShowFooter={false} >
                 {this.state.isWillLoad && <div className="vh-75">
                   <iframe scrolling="yes" className="w-100 h-100" src={item.url} ></iframe>
                 </div>
                 }
-              </ModalPopup>
+              </ModalPopupForm>
             </div>
             <p className="word-wrap p-2 text-center"><small>{item.name}</small></p>
           </div>
