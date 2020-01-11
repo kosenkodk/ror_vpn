69dae398d00417440e69808031935adf78f026af
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2020-01-12 01:42:06 +0300

    fix ticket's attachment modal popups

--- app/javascript/packs/app_pack/_components/admin/AttachmentImage.jsx
+++ app/javascript/packs/app_pack/_components/admin/AttachmentImage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ModalPopup } from '../../_components';
+import { ModalPopupForm } from '../../_components';
 
 class AttachmentImage extends React.Component {
   render() {
@@ -10,11 +10,11 @@ class AttachmentImage extends React.Component {
         <div className="col">
           <div className="h-100">
             <div className="embed-responsive embed-responsive-16by9">
-              <ModalPopup id={`modalForAttachmentImage${index}`} aImgClasses='card-img-top embed-responsive-item cover' aUrl={item.url} aImgSrc={item.url} aTitle={item.name} title={item.name} isShowFooter={false} >
+              <ModalPopupForm id={`modalForAttachmentImage${index}`} aImgClasses='card-img-top embed-responsive-item cover' aUrl={item.url} aImgSrc={item.url} aTitle={item.name} title={item.name} isShowFooter={false} >
                 <div className="text-center">
                   <img src={item.url} className="img-fluid mh-75" alt={item.name} />
                 </div>
-              </ModalPopup>
+              </ModalPopupForm>
             </div>
             <p className="word-wrap p-2 text-center"><small>{item.name}</small></p>
           </div>
