03764d11640b3abe0eac24d6568609d6b9588967
Dmitry K <kosenkodk@gmail.com>
2020-01-12 23:43:15 +0300

    api - get qr code url

--- spec/controllers/api/v1/user_mfa_session_controller_spec.rb
+++ spec/controllers/api/v1/user_mfa_session_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::UserMfaSessionController, type: :controller do
+  let(:user) {create(:user)}
+  before { sign_in_as(user) }
 
+  describe 'GET #new' do
+    it 'get qr code url' do
+      get :new
+      expect(response_json.keys).to eq(['notice', 'qr_code_url'])
+      expect(response_json['qr_code_url']).to eq(user.google_qr_uri)
+      expect(response).to be_successful
+    end
+  end
 end
