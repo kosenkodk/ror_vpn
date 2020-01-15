acea70297396d298b04f9201dd5a84afa146f03a
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2020-01-15 17:52:00 +0300

    test mode for custom switcher is not correct process.env.NODE_ENV display development mode instead of test

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -1,5 +1,6 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+process.env.NODE_ENV = process.env.NODE_ENV || 'test'
 
 const environment = require('./environment')
+const config = environment.toWebpackConfig()
 
-module.exports = environment.toWebpackConfig()
+module.exports = config
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
+puts "The Rails environment is running in #{ENV['RAILS_ENV']} mode!"
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'action_cable/testing/rspec'
