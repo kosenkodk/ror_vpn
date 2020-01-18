f06ffb31d28ef398693de7fc5c4874c5eb7d7ec8
Dmitry K <kosenkodk@gmail.com>
2020-01-18 23:17:28 +0300

    controller test

--- app/controllers/api/v1/signin_controller.rb
+++ app/controllers/api/v1/signin_controller.rb
@@ -3,8 +3,40 @@ class Api::V1::SigninController < Api::V1::ApiController
   # before_action :authorize_refresh_by_access_header!, only: [:destroy]
   # before_action :authorize_by_access_header!,only: [:destroy]
 
+  def signin_check_credentials
+    user = User.find_by!(email: params[:email])
+    if user.authenticate(params[:password])
+      render json: { }
+    else
+      render json: { }, status: :unauthorized
+    end
+  end
+
+  def signin_check_code
+    # user = current_user
+    user = User.find_by!(email: params[:email])
+
+    if (!user.google_authentic?(params[:code2fa]))
+      render json: { error: I18n.t("api.errors.invalid_code") }, status: :unauthorized
+      return
+    end
+
+    payload = { user_id: user.id, aud: [user.role] }
+    session = JWTSessions::Session.new(payload: payload,
+                                        refresh_by_access_allowed: true,
+                                        namespace: "user_#{user.id}")
+    tokens = session.login
+
+    response.set_cookie(JWTSessions.access_cookie,
+                        value: tokens[:access],
+                        httponly: true,
+                        secure: Rails.env.production?)
+    render json: { csrf: tokens[:csrf] }
+  end
+
   def create
     user = User.find_by!(email: params[:email])
+    
     if user.authenticate(params[:password])
       payload = { user_id: user.id, aud: [user.role] }
       session = JWTSessions::Session.new(payload: payload,
