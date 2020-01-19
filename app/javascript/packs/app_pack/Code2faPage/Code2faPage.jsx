037d5416efffdcf9ee6e4a1e8fc845401b689683
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2020-01-20 00:12:05 +0300

    add code2fa page and form

---
+++ app/javascript/packs/app_pack/Code2faPage/Code2faPage.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { I18n } from 'helpers';
+import { Code2faForm } from './Code2faForm';
+// import FlashMessages from './_components/FlashMessages';
+
+class Code2faPage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="container login">
+        <div className="featurette text-center vh-100">
+          <div className="row h-50">
+            <div className="col-md-12 align-self-end">
+              <h1 className="featurette-heading">
+                {I18n.t('pages.code2fa.title')}
+              </h1>
+              <p className="lead">
+                {I18n.t('pages.code2fa.check_device')}
+              </p>
+            </div>
+
+            {/* <div className="col-md-4 offset-md-4 text-center">
+              <FlashMessages error={this.state.error} notice={this.state.notice} />
+            </div> */}
+
+            <div className="col-md-8 offset-md-2 align-self-start">
+              <div className="text-right">
+                <Code2faForm />
+              </div>
+              <div className="row">
+                <div className="col-md-8 offset-md-2">
+                  {I18n.t("pages.code2fa.form.help.trusted_device")}
+                  <Link to="/trusted_device" className="trouble">{I18n.t("pages.code2fa.form.help.learn_more")}</Link>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-12">
+              {/* <%#= image_tag 'coming_soon/mars_with_spaceship.png', {class:'img-fluid'} #%> */}
+            </div>
+          </div>
+        </div>
+      </div>
+
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  const { loggingIn } = state.authentication;
+  return {
+    loggingIn
+  };
+}
+
+const connectedPage = connect(mapStateToProps)(Code2faPage);
+export { connectedPage as Code2faPage }; 
