344090e9ad436255d58d73a8b1a131fa054c93e8
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2020-01-24 00:42:50 +0300

    fix for mobiles (custom select files input)

--- app/javascript/packs/app_pack/_components/MessageForm.jsx
+++ app/javascript/packs/app_pack/_components/MessageForm.jsx
@@ -73,17 +73,18 @@ class MessageForm extends React.Component {
             */}
 
             <div className="form-group d-flex flex-column flex-sm-row justify-content-sm-between">
-              <div className="file row mb-3 mb-sm-0 mr-2">
+
+              <button id="contact_submit" className="btn btn-outline-primary mb-3 mb-sm-0 mr-2" disabled={this.props.loading ? true : false}>
+                {this.props.loading && <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>}
+                {' ' + I18n.t('pages.tickets.form.submit')}
+              </button>
+
+              <div className="file row mb-3 mb-sm-0 mr-auto">
                 <div className="upload-btn-wrapper col-12">
                   <button className="btn">{I18n.t('buttons.select_files')}</button>
                   <input type="file" name="attachments" onChange={this.onFilesChange} required={false} multiple={true} accept="application/pdf, image/*" />
                 </div>
               </div>
-
-              <button id="contact_submit" className="mr-auto btn btn-outline-primary mb-3 mb-sm-0" disabled={this.props.loading ? true : false}>
-                {this.props.loading && <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>}
-                {' ' + I18n.t('pages.tickets.form.submit')}
-              </button>
 
               {this.props.item &&
                 <button onClick={(e) => this.onTicketClose(e, this.props.item)} className='btn btn-outline-danger'
