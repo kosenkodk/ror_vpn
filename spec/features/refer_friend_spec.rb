604a54ae44724bf56dede10b37f0be06905cc9ba
Dmitry K <kosenkodk@gmail.com>
2020-02-15 11:36:12 +0300

    add feature test (sign up with refer link)

--- spec/features/refer_friend_spec.rb
+++ spec/features/refer_friend_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe 'Refer Friend', type: :feature, js: true do
   let(:email) {'email@ex.com'}
   let(:emails) { 'email@ex.com,email2@ex.com' }
   let!(:user) {create(:user)}
-  
+  let(:user_referred) { build(:user)}
+
   before {
     fsign_in_as(user)
     visit '/user/refer_friend'
@@ -19,6 +20,21 @@ RSpec.describe 'Refer Friend', type: :feature, js: true do
       
       # alert_have_text I18n.t('pages.refer_friend.send_invites.error')
       alert_have_text I18n.t('pages.refer_friend.send_invites.success')
+    end
+  end
+
+  
+  context 'signup' do
+    it 'with refer link' do
+      visit("/signup?refer=#{user.id}")
+      fill_in :email, with: user_referred.email
+      fill_in :password, with: user_referred.password
+      fill_in :password_confirmation, with: user_referred.password_confirmation
+      click_on(I18n.t('buttons.continue'))
+      user_referred = User.find_by(email: user_referred.email)
+      expect(user_referred.referrer).to eq(user) # todo
+      user.reload
+      expect(user.referrals).to contains(user_referred)
     end
   end
 end
