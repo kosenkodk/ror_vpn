99a6a45e4ea7f3c4a312d4df203832c7deaa8dda
Dmitry K <kosenkodk@gmail.com>
2020-02-17 21:35:31 +0300

    validate emails (send invites)

--- spec/controllers/api/v1/refer_friend_controller_spec.rb
+++ spec/controllers/api/v1/refer_friend_controller_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Api::V1::ReferFriendController, type: :controller do
   let(:user) { create(:user) }
   let(:access_cookie) { @tokens[:access] }
   let(:csrf_token) { @tokens[:csrf] }
+
   before {
     # JWTSessions.access_exp_time = 3600
     payload = { user_id: user.id }
@@ -14,44 +15,61 @@ RSpec.describe Api::V1::ReferFriendController, type: :controller do
     @tokens = session.login
   }
      
-  context 'success' do
+  describe 'logged in users' do
     before {
       request.cookies[JWTSessions.access_cookie] = access_cookie
       request.headers[JWTSessions.csrf_header] = csrf_token
     }
 
-    it 'generate refer link' do
-      get :link
-      # expect(response_json).to eq({})
-      expect(response_json['refer_link']).to eq(user.get_refer_link)
-      expect(response).to have_http_status(:success)
+    describe 'generate refer link' do
+      context 'success' do
+        it do
+          get :link
+          # expect(response_json).to eq({})
+          expect(response_json['refer_link']).to eq(user.get_refer_link)
+          expect(response).to have_http_status(:success)
+        end
+      end
     end
 
-    it 'send refer link to emails' do
-      post :create, params: {emails: 'email@ex.com,email2@ex.com'}
-      expect(response).to have_http_status(:success)
+    describe 'send refer link' do
+      context 'success' do
+        it 'send refer link to emails' do
+          post :create, params: {emails: 'email@ex.com,email2@ex.com'}
+          expect(response).to have_http_status(:success)
+        end
+
+        it 'send refer link to email' do
+          post :create, params: {emails: 'email@ex.com'}
+          expect(response).to have_http_status(:success)
+        end
+      end
+
+      context 'failure' do
+        it 'with empty email' do
+          post :create, params: {emails: ''}
+          # expect(response).to have_http_status(:bad_request)
+          expect(response_json['error']).to eq(I18n.t('api.errors.email_blank'))
+        end
+
+        it 'with invalid email' do
+          post :create, params: {emails: 'e@'}
+          # expect(response).to have_http_status(:bad_request)
+          expect(response_json['error']).to eq(I18n.t('api.errors.invalid_email'))
+        end
+
+        it 'with no params' do
+          post :create
+          # expect(response).to have_http_status(:bad_request)
+          expect(response_json['error']).to eq(I18n.t('api.errors.email_blank'))
+        end
+      end
     end
 
-    it 'send refer link to email' do
-      post :create, params: {emails: 'email@ex.com'}
-      expect(response).to have_http_status(:success)
+    context 'if reffered friend bought a paid subscription' do
+      it 'add bonus: free 1/1/2 month(-s) trial of paid subscription [1mo/3mo/year] for both users'
+      it 'upgrade referrer from free to the paid subscription (1-2 mo for free)'
+      it 'display a bonus message in notifications for both users'
     end
-
-    it 'send refer link to empty email' do
-      post :create, params: {emails: ''}
-      expect(response).to have_http_status(:success)
-    end
-
-    it 'send refer link with no params' do
-      post :create
-      expect(response).to have_http_status(:success)
-    end
-
-  end
-
-  context 'if reffered friend bought a paid subscription' do
-    it 'add bonus: free 1/1/2 month(-s) trial of paid subscription [1mo/3mo/year] for both users'
-    it 'upgrade referrer from free to the paid subscription (1-2 mo for free)'
-    it 'display a bonus message in notifications for both users'
   end
 end
