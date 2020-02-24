a83bc6c23618eff0e2dfffd4fa1939402be59ecf
Dmitry K <kosenkodk@gmail.com>
2020-02-24 13:59:58 +0300

    Add info block

--- app/javascript/packs/app_pack/_components/admin/InfoBlock.jsx
+++ app/javascript/packs/app_pack/_components/admin/InfoBlock.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class InfoBlock extends React.Component {
   render() {
+    const { children, linkTitle, linkUrl, title, text, optionalCssClasses } = this.props;
     return (
-      <div className="border-left-pink mt-0">
-        <p className="mt-0 mb-2">
-          {this.props.children}
-        </p>
+      <div className={`border-left-pink mt-0 ${optionalCssClasses}`}>
+        {children ? children :
+          <React.Fragment>
+            {title && <h6 id="caveat-with-anchors">{title}</h6>}
+            {text && <p className="mt-0 mb-2">{text}
+              {linkTitle && <Link to={`${linkUrl || '#'}`} className="mt-1 text-blue"> {linkTitle}</Link>}
+            </p>}
+
+          </React.Fragment>
+        }
       </div>
     );
   }
