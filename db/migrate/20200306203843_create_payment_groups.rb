813a3a871a6ed6d2e1c794e2fb3c5dbae40670d4
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2020-03-07 17:52:33 +0300

    remove payment method groups

--- db/migrate/20200306203843_create_payment_groups.rb
+++ db/migrate/20200306203843_create_payment_groups.rb
@@ -1,7 +1,10 @@
 class CreatePaymentGroups < ActiveRecord::Migration[6.0]
   def change
     create_table :payment_groups do |t|
-
+      t.string :title
+      t.integer :order
+      t.boolean :is_on_main_page
+      t.boolean :is_draft
       t.timestamps
     end
   end
