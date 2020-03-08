01808070b01ff6ad75c8823116d8624642fa8a1d
Dmitry K <kosenkodk@gmail.com>
2020-03-08 15:01:18 +0300

    payment group list api

--- spec/controllers/api/v1/payment_methods_controller_spec.rb
+++ spec/controllers/api/v1/payment_methods_controller_spec.rb
@@ -1,14 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::PaymentMethodsController, type: :controller do
-  let!(:payment_method) { create(:payment_method, title: 'free', is_for_signup: true)}
+  let(:payment_group_title) { 'Cryptocurrencies'}
+  let(:payment_method_title) { 'Bitcoin'}
+  let!(:payment_group) { create(:payment_group, title: payment_group_title, is_on_main_page: true)}
+  let!(:payment_method) { create(:payment_method, title: payment_method_title, is_for_signup: true)}
   describe 'index' do
     it 'for signup page' do
       get 'for_signup'
       expect(response).to have_http_status(200)
       expect(response_json[0].keys.sort).to eq(['active_class', 'icon_urls', 'id', 'title'])
-      expect(response_json[0].values).to include('free')
-      expect(response_json[0]['title']).to eq('free')
+      expect(response_json[0].values).to include(payment_method_title)
+      expect(response_json[0]['title']).to eq(payment_method_title)
+    end
+    it 'payment groups' do
+      payment_method.payment_group_id = payment_group
+      get 'groups'
+      expect(response).to have_http_status(200)
+      expect(response_json[0].values).to include(payment_group_title)
+      expect(response_json[0].values).to include(payment_method_title)
     end
   end
 end
