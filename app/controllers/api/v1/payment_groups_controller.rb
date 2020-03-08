1a7f3bfac718d407b18dd98bfa7470c23a58e93e
Dmitry K <kosenkodk@gmail.com>
2020-03-08 18:53:35 +0300

    join payment methods for api

--- app/controllers/api/v1/payment_groups_controller.rb
+++ app/controllers/api/v1/payment_groups_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::PaymentGroupsController < ApplicationController
   def index
     items = PaymentGroup.all
-    render json: items
+    render json: items.as_json(
+      # only: [:id, :title],
+      include: :payment_methods
+      # except: [:created_at, :updated_at]
+    )
   end
 end
