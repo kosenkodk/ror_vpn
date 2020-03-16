33a29e64f8d3297a76ac2de09475712a43909309
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2020-03-16 23:26:11 +0300

    add info block with warning icon

--- app/javascript/packs/app_pack/PaymentsPage/PaymentsPage.jsx
+++ app/javascript/packs/app_pack/PaymentsPage/PaymentsPage.jsx
@@ -8,6 +8,7 @@ import { InfoBlock } from '../_components/admin';
 import { ModalPopupForm } from '../_components/ModalPopupForm';
 import { accountActions, globalActions, alertActions } from '../_actions';
 import { FormDataAsJsonFromEvent } from '../_helpers';
+import icInfoSrc from 'images/admin/ic_warning.svg';
 
 class PaymentsPage extends React.Component {
   constructor(props) {
@@ -25,41 +26,53 @@ class PaymentsPage extends React.Component {
     const { tariff_plan } = user;
     return (
       <div className="container-fluid payments">
-        <div>
-          <h1>{I18n.t('pages.payments.payment_methods.title')}</h1>
-          <InfoBlock>
-            If you wish to pay by credit card, you can add your card below. Learn about other payment options.
-            <Link to="#" className="mt-1 text-blue"> Learn more</Link>
-          </InfoBlock>
+        <div className="row">
+          <div className="col-md-12 col-lg-8">
+            <h1>{I18n.t('pages.payments.payment_methods.title')}</h1>
+            <InfoBlock>
+              If you wish to pay by credit card, you can add your card below. Learn about other payment options.
+              <Link to="#" className="mt-1 text-blue"> Learn more</Link>
+            </InfoBlock>
 
-          <div className=" ">
-            <button className="btn btn-pink mr-2">{I18n.t('pages.payments.payment_methods.add_bank_card')}</button>
-            <button className="btn btn-pink">{I18n.t('pages.payments.payment_methods.add_paypal')}</button>
+            <div className=" ">
+              <button className="btn btn-pink mr-2">{I18n.t('pages.payments.payment_methods.add_bank_card')}</button>
+              <button className="btn btn-pink">{I18n.t('pages.payments.payment_methods.add_paypal')}</button>
+            </div>
+
+            <InfoBlock optionalCssClasses="my-5">
+              <div className="row">
+                <img src={icInfoSrc} className="col-1 img-fluid" />
+                <p className="col">
+                  To settle your invoice(s) via Crypto or Qiwi, you need to manually trigger the pay button in “Invoices” section.
+                </p>
+              </div>
+            </InfoBlock>
+
+
+            <div id="subscriptions" className="invoices">
+              <h1>{I18n.t('pages.payments.invoices.title')}</h1>
+              <InfoBlock>
+                You can customize and download your invoices for accounting purposes.
+              </InfoBlock>
+            </div>
+
+            <ModalPopupForm
+              // onClose={this.onModalClose}
+              id='changePlan'
+              isForm={this.state.isForm}
+              isHideBtn={true}
+              isNextBtnOnly={true}
+              // onBtnSave={(e) => this.setStep(e, step + 1)}
+              title={this.state.title}
+              btnCloseText={I18n.t('buttons.cancel')}
+              btnSaveText={I18n.t('buttons.next')}
+              btnClasses={''}>
+              <InfoBlock>
+                Your current subscription: {(user && user.tariff_plan && user.tariff_plan.title)}
+              </InfoBlock>
+            </ModalPopupForm>
           </div>
         </div>
-        <div id="subscriptions" className="invoices">
-          <h1>{I18n.t('pages.payments.invoices.title')}</h1>
-          <InfoBlock>
-            You can customize and download your invoices for accounting purposes.
-          </InfoBlock>
-        </div>
-
-        <ModalPopupForm
-          // onClose={this.onModalClose}
-          id='changePlan'
-          isForm={this.state.isForm}
-          isHideBtn={true}
-          isNextBtnOnly={true}
-          // onBtnSave={(e) => this.setStep(e, step + 1)}
-          title={this.state.title}
-          btnCloseText={I18n.t('buttons.cancel')}
-          btnSaveText={I18n.t('buttons.next')}
-          btnClasses={''}>
-          <InfoBlock>
-            {/* A 20% discount for annual billing has been automatically applied. */}
-            Your current subscription: {(user && user.tariff_plan && user.tariff_plan.title)}
-          </InfoBlock>
-        </ModalPopupForm>
       </div>
     );
   }
