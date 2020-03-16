d11f74c81f3c7bcbe0d9593a367e6124fe88fa32
Dmitry K <kosenkodk@gmail.com>
2020-03-16 15:02:14 +0300

    fix tests

--- spec/models/payment_method_spec.rb
+++ spec/models/payment_method_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe PaymentMethod, type: :model do
   it 'get payment methods for sign up page only' do
     payment_method = create(:payment_method, is_for_signup: true)
-    payment_method2 = create(:payment_method)
+    payment_method2 = create(:payment_method, is_for_signup: false)
     expect(PaymentMethod.for_signup.count).to eq(1)
   end
   it 'check payment id' do
