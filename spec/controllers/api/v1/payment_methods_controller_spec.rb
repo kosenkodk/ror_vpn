37d3d720c34d5c8820c49bad13e4a57acde26cc1
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2020-03-17 16:31:33 +0300

    create payment method for logged in users only

--- spec/controllers/api/v1/payment_methods_controller_spec.rb
+++ spec/controllers/api/v1/payment_methods_controller_spec.rb
@@ -3,7 +3,20 @@ require 'rails_helper'
 RSpec.describe Api::V1::PaymentMethodsController, type: :controller do
   let(:payment_method_title) { 'Bitcoin' }
   let!(:payment_method) { create(:payment_method, title: payment_method_title, is_for_signup: true)}
-  describe 'index' do
+  let(:user) {create(:user)}
+  let(:access_cookie) { @tokens[:access] }
+  let(:csrf_token) { @tokens[:csrf] }
+  
+  before {
+    payload = { user_id: user.id }
+    session = JWTSessions::Session.new(payload: payload, 
+      refresh_by_access_allowed: true, 
+      namespace: "user_#{user.id}"
+    )
+    @tokens = session.login
+  }
+  
+  describe 'get payment methods' do
     it 'for signup page' do
       get 'for_signup'
       expect(response).to have_http_status(200)
@@ -12,12 +25,35 @@ RSpec.describe Api::V1::PaymentMethodsController, type: :controller do
       expect(response_json[0]['title']).to eq(payment_method_title)
     end
   end
-  describe 'create a payment method' do
-    context 'success' do
-      it do
-        post :create, params: {title:'paypal'}
-        expect(response).to have_http_status(200)
+  
+  describe 'logged in users' do
+    describe 'create a payment method' do
+      before {
+        request.cookies[JWTSessions.access_cookie] = access_cookie
+        request.headers[JWTSessions.csrf_header] = csrf_token
+      }
+      context 'success' do
+        it do
+          post :create, params: {title: 'paypal'}
+          expect(response).to have_http_status(200)
+        end
+      end
+      context 'failure' do
+        it 'with empty params' do
+          post :create
+          expect(response).to have_http_status(422)
+        end
       end
     end
   end
+
+  describe 'guests' do
+    describe 'can not create a payment method' do
+      it do
+        post :create, params: {title: 'paypal'}
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+  end
+  
 end
