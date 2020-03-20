a97d0005b152f6dfedcf3e42cf7966a033558d3e
Dmitry K <kosenkodk@gmail.com>
2020-03-20 22:22:44 +0300

    card code validator

--- app/javascript/packs/app_pack/DashboardPage/BankCard.jsx
+++ app/javascript/packs/app_pack/DashboardPage/BankCard.jsx
@@ -3,11 +3,88 @@ import { InfoBlock } from '../_components/admin';
 import { NavHashLink as Link } from 'react-router-hash-link';
 
 class BankCard extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      fullnameError: '',
+      form: {
+        name: {
+          value: '',
+          valid: true,
+          rules: {
+            minLength: 3
+          }
+        },
+        card_code: {
+          value: '',
+          valid: true,
+          rules: {
+            maxLength: 3,
+            isNumber: true
+          }
+        }
+      }
+    }
+  }
+
+  onChangeHandler = (key, value) => {
+    this.setState(state => ({
+      ...state,
+      form: {
+        ...state.form,
+        [key]: {
+          ...state.form[key],
+          value,
+          valid: this.validate(value, state.form[key].rules)
+        }
+      }
+    }))
+  }
+
+  validate = (value, rules) => {
+    let valid = true
+
+    for (let key in rules) {
+      switch (key) {
+
+        case 'minLength':
+          valid = valid && this.minLengthValidator(value, rules[key])
+          break
+        case 'maxLength':
+          valid = valid && this.maxLengthValidator(value, rules[key])
+          break
+        case 'isNumber':
+          valid = valid && this.isNumberValidator(value)
+          break
+
+        default: break
+      }
+    }
+
+    return valid
+  }
+
+  minLengthValidator = (value, rule) => (value.length >= rule)
+  maxLengthValidator = (value, rule) => (value.length <= rule)
+  isNumberValidator = value => !isNaN(parseFloat(value)) && isFinite(value)
+
+
   onInputChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
+    // console.log('this.state', this.state)
+    if (this.state.full_name && this.state.full_name.length > 1) {
+      this.setState({ fullnameError: '' })
+    } else {
+      this.setState({ fullnameError: 'Name on card required' })
+      return
+    }
     this.props.onInputChange(e)
   }
+
   render() {
     const { countries, item, defaultCountryCode } = this.props
+    const { form } = this.state
     return (<React.Fragment>
       <div className="form-group row">
         <label className="col-sm-4 col-form-label" htmlFor="full_name">
@@ -15,6 +92,10 @@ class BankCard extends React.Component {
         </label>
         <div className="col-sm-6">
           <input type="text" name="full_name" aria-describedby="full_name" required={true} className="form-control" placeholder='' onChange={this.onInputChange} />
+          {this.state.fullnameError && <small id="fullNameInline" className="text-muted text-red">
+            {this.state.fullnameError}
+          </small>
+          }
         </div>
         <div className="col-sm-2"></div>
       </div>
@@ -25,6 +106,7 @@ class BankCard extends React.Component {
         </label>
         <div className="col-sm-6">
           <input type="text" name="card_no" aria-describedby="card_no" required={true} className="form-control" placeholder='' onChange={this.onInputChange} />
+
         </div>
         <div className="col-sm-2"></div>
       </div>
@@ -38,6 +120,13 @@ class BankCard extends React.Component {
         </div>
         <div className="col-sm-3">
           <input type="text" name="card_code" aria-describedby="card_details" required={true} className="form-control" placeholder='Security code' onChange={this.onInputChange} />
+          <input type="text" name="card_code" aria-describedby="card_details" required={true} className="form-control" placeholder='Security code'
+            value={form.card_code.value} onChange={e => this.onChangeHandler('card_code', e.target.value)}
+          />
+          {!form.card_code.valid && <small id="cardCodeInline" className="text-muted text-red">
+            Invalid Card Code
+          </small>
+          }
         </div>
         <div className="col-sm-2"></div>
       </div>
