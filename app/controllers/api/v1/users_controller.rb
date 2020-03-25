468afb83eaa720fe094e9b6dd864b50a222a76c4
Dmitry K <kosenkodk@gmail.com>
2020-03-25 23:02:09 +0300

    todo: add is refer bonus used

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,14 +11,16 @@ class Api::V1::UsersController < Api::V1::ApiController
       plan = TariffPlan.find(plan_id)
       current_user.tariff_plan = plan
       current_user.prolongate_on(1.month)
-      if User.exists?(current_user.referrer_id)
+      if current_user.referrer_id && User.exists?(current_user.referrer_id)
         @user_referrer = User.find(current_user.referrer_id)
         @user_referrer.tariff_plan = current_user.tariff_plan # если  1 месяц уже был на одном плане то + 1 месяц на другом ?
         if current_user.tariff_plan.title === 'Plan for 1 year'
           @user_referrer.prolongate_on(2.month)
+          current_user.referrer_id = nil
         elsif current_user.tariff_plan.title === 'Free'
         else
           @user_referrer.prolongate_on(1.month)
+          current_user.referrer_id = nil
         end
         if @user_referrer.save
           # todo: send success message to referrer
