01202dde83c33b254608c089a8450904aee2f64e
Dmitry K <kosenkodk@gmail.com>
2020-03-25 23:33:59 +0300

    add plan checker to user model

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -14,14 +14,12 @@ class Api::V1::UsersController < Api::V1::ApiController
       if current_user.referrer_id && User.exists?(current_user.referrer_id)
         @user_referrer = User.find(current_user.referrer_id)
         @user_referrer.tariff_plan = current_user.tariff_plan # если  1 месяц уже был на одном плане то + 1 месяц на другом ?
-        if current_user.tariff_plan.title === 'Plan for 1 year'
+        if current_user.is_plan_yearly
           @user_referrer.prolongate_on(2.month) if !current_user.is_refer_bonus_used
-          # current_user.referrer_id = nil
           current_user.is_refer_bonus_used = true
-        elsif current_user.tariff_plan.title === 'Free'
+        elsif current_user.is_plan_free
         else
           @user_referrer.prolongate_on(1.month) if !current_user.is_refer_bonus_used
-          # current_user.referrer_id = nil
           current_user.is_refer_bonus_used = true
         end
         if @user_referrer.save
