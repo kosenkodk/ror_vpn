77c65e3953c381b0a905ff1fa828e05321aa28ea
Dmitry K <kosenkodk@gmail.com>
2020-04-01 00:27:13 +0300

    generate test invoice pdf without styles

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,6 @@
 class Invoice < ApplicationRecord
+  
+  before_create :generate_pdf
   after_save_commit :check_status
 
   belongs_to :user
@@ -13,7 +15,76 @@ class Invoice < ApplicationRecord
     { id: id, no: no, amount: amount, currency: currency, invoice_type: invoice_type, status: status, created_at_humanize: created_at_humanize, details_from: details_from }
   end
  
+  def to_pdf
+    # # todo: increase perfomance
+    # # layout = Erubis::Eruby.new(File.read(Rails.root.join('app/views/api/v1/invoices/invoice_pdf_layout.html.erb')))
+    # # body = Erubis::Eruby.new(File.read(Rails.root.join('app/views/api/v1/invoices/invoice_pdf_body.html.erb')))
+    # layout = File.read(Rails.root.join('app/views/api/v1/invoices/invoice_pdf_layout.html.erb'))
+    # body = File.read(Rails.root.join('app/views/api/v1/invoices/invoice_pdf_body.html.erb'))
+    # # body_html = body.result(binding)
+    # # pdf_html = layout.result(body: body_html) # replace `yield` in layout with `body`
+    # pdf_html = "<h1>Invoice</h1>"
+    # # pdf_html = ActionController::Base.new.render_to_string(template: 'app/views/invoices/invoice.html.erb', layout: 'app/views/api/v1/invoices/invoice_pdf_layout.html')
+
+    # pdf = WickedPdf.new.pdf_from_string(pdf_html)
+
+    generate_pdf
+  end
+
   private
+  def generate_pdf
+    pdf_html = """
+    <div>
+      <div className='row'>
+        <div className='col-4 offset-4 text-center'>
+          Vega VPN <br />
+        123 Grienfield Drive<br />
+        Yardville, NM 49990<br />
+        (555) 555-0198<br />
+        sale@vega.com<br />
+        </div>
+      </div>
+      <div className='row mt-30'>
+        <div className='col'>
+          <NewLineToBr>#{self.details_from}</NewLineToBr>
+        </div>
+        <div className='col'>
+          Invoice #: #{self.no}<br />
+          Date: #{self.created_at_humanize}<br />
+        </div>
+      </div>
+
+      <table className='table mt-30'>
+        <thead>
+          <tr>
+            <th className='font-weight-bold'>Services</th>
+            <th className='font-weight-bold'>Date</th>
+            <th className='font-weight-bold'>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+            <React.Fragment>
+              <tr>
+                <td>#{self.title}</td>
+                <td>#{self.created_at_humanize}</td>
+                <td>#{self.currency}#{self.amount}</td>
+              </tr>
+              <tr>
+                <td colSpan='2' className='font-weight-bold'>Total</td>
+                <td className='font-weight-bold'>#{self.currency}#{self.amount}</td>
+              </tr>
+            </React.Fragment>
+        </tbody>
+      </table>
+    </div>
+    """
+    # pdf_html = ActionController::Base.new.render_to_string(template: 'app/views/invoices/invoice.html.erb', layout: 'app/views/api/v1/invoices/invoice_pdf_layout.html')
+    pdf = WickedPdf.new.pdf_from_string(pdf_html)
+    filename = "invoice#{DateTime.try(:now).try(:strftime, "%d%m%Y")}.pdf"
+    self.pdf.attach(io: StringIO.new(pdf), filename: filename)
+    return pdf
+  end
+
   def check_status
     if self.status === 'paid'
       if self.user
