176c402412c6b7b6c79cdee359b520a4e7077773
Dmitry K <kosenkodk@gmail.com>
2020-04-01 20:47:45 +0300

    invoice modal popup with buttons: add download, open in new tab and labels: pdf size and date creation

--- app/javascript/packs/app_pack/PaymentsPage/InvoiceDetails.jsx
+++ app/javascript/packs/app_pack/PaymentsPage/InvoiceDetails.jsx
@@ -1,17 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { NewLineToBr } from '../_components';
+import icDownloadSrc from 'images/icons/ic_download.svg';
+import icOpenInNewTabSrc from 'images/icons/ic_open_in_new_tab.svg';
 
 class InvoiceDetails extends React.Component {
+
+
+  onDownloadInvoice = (e, invoice) => {
+    e.preventDefault()
+    // window.open(invoice.pdf_url, '_blank') // open in a new tab
+    window.open(invoice.pdf_url) // open in a new tab
+    // window.location.href = invoice.pdf_url; // open in the current page
+  }
+
   render() {
     const { invoice, user } = this.props
     return (
       <React.Fragment>
         {invoice &&
-          <div className="vh-50">
-            <iframe scrolling="yes" className="w-100 h-100" src={invoice.pdf_url} ></iframe>
-          </div>
+          <React.Fragment>
+            <div className="vh-50">
+              <iframe scrolling="yes" className="w-100 h-100" src={invoice.pdf_url} ></iframe>
+            </div>
+            {/* <div className="row justify-content-center align-items-center"> */}
+            <div className="row text-center mt-10">
+              <div className="col">
+                {invoice.created_at_humanize} - {(invoice.pdf_size > 0) && `${invoice.pdf_size / 1000} KB`}
+              </div>
+              <div className="col">
+                <a href={invoice.pdf_url} target="_blank">
+                  <img src={icOpenInNewTabSrc} className="img-fluid" />
+                &nbsp;
+                Open in new tab
+              </a>
+              </div>
+              <div className="col">
+                <a onClick={(e) => this.onDownloadInvoice(e, invoice)} className="px-1" href="#"
+                // href={invoice.pdf_url} target="_blank"
+                >
+                  <img src={icDownloadSrc} className="img-fluid" />
+                &nbsp;
+                Download
+              </a>
+              </div>
+            </div>
+          </React.Fragment>
         }
       </React.Fragment>
     )
