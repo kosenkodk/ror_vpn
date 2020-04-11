894ba38d7a5461879fda0880bb287461741f65d8
Dmitry K <kosenkodk@gmail.com>
2020-04-11 17:13:11 +0300

    notifications with auth

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::NotificationsController < Api::V1::ApiController
+  before_action :authorize_access_request!
+
   def index
     messages = Message.where(user_id: current_user.id)
-    render json: messages
+    render json: {notifications: messages}
   end
 end
--- spec/controllers/api/v1/notifications_controller_spec.rb
+++ spec/controllers/api/v1/notifications_controller_spec.rb
@@ -2,10 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::NotificationsController, type: :controller do
   let!(:user) { create(:user) }
-  let!(:notification) { create(:message, user_id: user.id) }
+  let!(:user2) { create(:user) }
+  let!(:user_notification) { create(:message, user_id: user.id) }
+  let!(:user2_notification) { create(:message, user_id: user2.id) }
 
   it 'display user\'s invoices' do
+    sign_in_as(user)
     get :index
-    expect(response_json.count).to eq(1)
+    expect(response_json['notifications'][0]['text']).to include(user_notification.text)
+    expect(response_json['notifications'].count).to eq(1)
   end
 end
