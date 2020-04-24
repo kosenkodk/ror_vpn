176142f6a269c29e2febe1ad276678271c95c615
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2020-04-24 20:36:58 +0300

    add open vpn urls

--- web-app/app/controllers/api/v1/configs_controller.rb
+++ web-app/app/controllers/api/v1/configs_controller.rb
@@ -85,7 +85,8 @@ class Api::V1::ConfigsController < Api::V1::ApiController
   private
   def get_vpn_url
     # @vpn_url = Rails.application.credentials.config.vpn_url
-    @vpn_url = Rails.application.credentials.vpn_url
+    # @vpn_url = Rails.application.credentials.vpn_url
+    @vpn_url = ENV.fetch("OPENVPN_PKI_URL")
     # @vpn_url = Rails.application.credentials.vpn[:url]
   end
 end
