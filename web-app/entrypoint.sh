21324e0cbfa12fc05a858b4cb33a8f740807fe6c
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2020-04-24 12:26:54 +0300

    dev mode

--- web-app/entrypoint.sh
+++ web-app/entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /data/tmp/pids/server.pid
 
+export NODE_OPTIONS="--max-old-space-size=4096"
 # bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:setup
 # bundle exec rake assets:precompile
-export NODE_OPTIONS="--max-old-space-size=4096"
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
