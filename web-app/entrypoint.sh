fedde0ede3426e3225b99d7962db06fec9b311e5
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2020-04-24 10:08:05 +0300

    export node options

--- web-app/entrypoint.sh
+++ web-app/entrypoint.sh
@@ -6,6 +6,7 @@ rm -f /data/tmp/pids/server.pid
 
 # bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:setup
 # bundle exec rake assets:precompile
+export NODE_OPTIONS="--max-old-space-size=4096"
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
