d1bca9403abb5bc6a70aaa60827fe6f963a2b170
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2020-04-25 12:58:33 +0300

    docker with webpacker

--- web-app/entrypoint.sh
+++ web-app/entrypoint.sh
@@ -8,5 +8,8 @@ export NODE_OPTIONS="--max-old-space-size=4096"
 # bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:setup
 # bundle exec rake assets:precompile
 
+# yarn install --check-files
+# bin/webpack-dev-server --host 0.0.0.0 --hot
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
