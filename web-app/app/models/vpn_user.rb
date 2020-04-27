ad78ea2247230944eded9c87e7df2a51764e7555
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2020-04-27 21:47:14 +0300

    delete vpn_user model

--- web-app/app/models/vpn_user.rb
+++
@@ -1,6 +0,0 @@
-class VpnUser
-  include Mongoid::Document
-  field :vpn_login, type: String
-  field :vpn_password, type: String
-  field :vpn_enabled, type: Mongoid::Boolean
-end
--- web-app/config/application.rb
+++ web-app/config/application.rb
@@ -23,5 +23,9 @@ module VegaVPN
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # config.generators do |g|
+    #   g.orm :mongoid
+    # end
   end
 end
