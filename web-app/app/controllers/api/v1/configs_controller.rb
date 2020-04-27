f5c81a67167aaf92b17b5ce2b680beaaa2c6bc09
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2020-04-27 23:02:02 +0300

    create user (if does not exist) in mongo db of ovpn server

--- web-app/app/controllers/api/v1/configs_controller.rb
+++ web-app/app/controllers/api/v1/configs_controller.rb
@@ -31,18 +31,22 @@ class Api::V1::ConfigsController < Api::V1::ApiController
   end
 
   def create_user user
-    url = "#{@vpn_url}/server/users/create"
-    uri = URI(url)
-    http = Net::HTTP.new(uri.host, uri.port)
-    # http.use_ssl = true
+    if VpnUser.where(vpn_login:user.email).count == 0
+      VpnUser.create(vpn_login:user.email, vpn_password: 'test', vpn_enabled: true)
+    end
+    # # create user via api
+    # url = "#{@vpn_url}/server/users/create"
+    # uri = URI(url)
+    # http = Net::HTTP.new(uri.host, uri.port)
+    # # http.use_ssl = true
 
-    request = Net::HTTP::Post.new(uri.path, {'Content-Type' => 'application/json'})
-    # request.body = { login: user.email, password: user }.to_json
-    request.body = { login: user.email }.to_json
+    # request = Net::HTTP::Post.new(uri.path, {'Content-Type' => 'application/json'})
+    # # request.body = { login: user.email, password: user }.to_json
+    # request.body = { login: user.email }.to_json
 
-    response = http.request(request)
-    body = response.body
-    # body = JSON.parse(response.body)
+    # response = http.request(request)
+    # body = response.body
+    # # body = JSON.parse(response.body)
   end
 
   def get_ca
--- web-app/spec/models/vpn_user_spec.rb
+++ web-app/spec/models/vpn_user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe VpnUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it do
+    item = VpnUser.create(vpn_login:'test@email.com', vpn_password: 'test', vpn_enabled: true)
+    expect(item.vpn_enabled).to eq(true)
+  end
 end
