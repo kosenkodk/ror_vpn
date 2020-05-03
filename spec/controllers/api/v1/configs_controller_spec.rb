bb69e6ef5481cfad5be7a1503430268698e07bda
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2020-05-03 14:55:57 +0300

    add config controller spec

--- spec/controllers/api/v1/configs_controller_spec.rb
+++ spec/controllers/api/v1/configs_controller_spec.rb
@@ -3,11 +3,15 @@ require 'rails_helper'
 RSpec.describe Api::V1::ConfigsController, type: :controller do
   describe 'ovpn configs' do
     let(:user) { create(:user) }
+    let(:config) { create(:config) }
     describe 'logged in users' do
       before { sign_in_as(user) }
       it 'get from vpn server' do
-        get :get
+        get :show, params: {id: config.id}
         expect(response).to have_http_status(:success)
+        vpn_user = VpnUser.find_by(vpn_login: user.email)
+        expect(vpn_user.vpn_login).to eq(user.email)
+        expect(vpn_user.vpn_enabled).to eq(true)
       end
     end
   end
