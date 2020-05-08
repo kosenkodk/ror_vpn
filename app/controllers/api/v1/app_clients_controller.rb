f67c993c95cba8cedf4a6c2568ff111b9929ea39
Dmitry K <kosenkodk@gmail.com>
2020-05-08 18:43:06 +0300

    app client order by id

--- app/controllers/api/v1/app_clients_controller.rb
+++ app/controllers/api/v1/app_clients_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::AppClientsController < Api::V1::ApiController
   # before_action :authorize_access_request!
 
   def index
-    clients = AppClient.order(title: :asc)
+    clients = AppClient.all
     render json: {clients: clients}
   end
 end
