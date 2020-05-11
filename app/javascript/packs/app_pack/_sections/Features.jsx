753b31ef838eb15cd69f20ccde0e1fce49700b98
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2020-05-11 19:32:33 +0300

    remove unused image imports

--- app/javascript/packs/app_pack/_sections/Features.jsx
+++ app/javascript/packs/app_pack/_sections/Features.jsx
@@ -1,30 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Feature from './Feature'
-import { withRouter } from "react-router-dom";
-// import imgFeaturesSrc from 'images/compare/features.png';
+import { withRouter } from "react-router-dom"
 
 class Features extends React.Component {
 
   constructor(props) {
-    super(props);
+    super(props)
     let features = props.features && props.features.length > 0 ? props.features : []
 
-    this.state = {
-      features: features
-    };
+    this.state = { features: features }
   }
 
   render() {
     const { features } = this.state;
     return (
       <div id="features" className="features row">
-        {/* <div className="row"><img src={imgFeaturesSrc} className="col img-fluid" /></div> */}
         {
           features.map(item => (
             <div key={item.id} className="col-xs-12 col-sm-6 col-md-4">
               <Feature {...item} />
-              {/* <Feature title={item.title} /> */}
             </div>
           ))
         }
@@ -34,11 +29,9 @@ class Features extends React.Component {
 
   componentDidMount() {
     if (this.props.features && this.props.features.length > 0) {
-      // console.log('getting data from props ...')
       // this.setState({ features: this.props.features })
       return;
     }
-    // console.log('getting data from api...')
 
     const url = "api/v1/features";
     fetch(url)
@@ -61,4 +54,3 @@ Features.propTypes = {
 };
 
 export default withRouter(Features)
-// export default Features
--- app/javascript/packs/app_pack/_sections/SecurityAndTrust.jsx
+++ app/javascript/packs/app_pack/_sections/SecurityAndTrust.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
-// import imgSecuritySrc from 'images/compare/security_and_trust_img_with_text'
 
 class SecurityAndTrust extends React.Component {
   render() {
     return (
       <div id="security_and_trust" className="row card d-flex flex-column">
-        {/* <img src={imgSecuritySrc} className=" img-fluid" /> */}
-
         <div className="row no-gutters d-flex align-items-center">
           <div className="col-md-4 offset-md-4">
             <div className="card-body">
@@ -21,7 +18,7 @@ class SecurityAndTrust extends React.Component {
             <div className="card-body">
               <h5 className="card-title">Private</h5>
               <p className="card-text">Keep your real location and Unique IP address hidden, prevent Anyone from viewing your
-                Internet activity.
+              Internet activity.
               </p>
               {/* <!-- <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p> --> */}
             </div>
@@ -30,7 +27,7 @@ class SecurityAndTrust extends React.Component {
             <div className="card-body">
               <h5 className="card-title">Secure</h5>
               <p className="card-text">Use WiFi Hotspots Safe From cyber criminals, block incoming threats and DDoS Attacks With
-                Our Firewall.
+              Our Firewall.
             </p>
             </div>
           </div>
