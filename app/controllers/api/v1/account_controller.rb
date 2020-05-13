9764997541c8b9b22e820377384a3c7878d020a8
Dmitry K <kosenkodk@gmail.com>
2020-05-13 22:03:45 +0300

    account controller - change email subscriptions

--- app/controllers/api/v1/account_controller.rb
+++ app/controllers/api/v1/account_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::AccountController < Api::V1::ApiController
   before_action :authorize_access_request!
-  before_action :find_user, only: [:change_password, :change_email, :delete, :cancel]
+  before_action :find_user, only: [:change_password, :change_email, :change_email_subscriptions, :delete, :cancel]
   KEYS = [:password, :password_confirmation, :password_old].freeze
   EMAIL_KEYS = [:email].freeze
 
@@ -61,6 +61,14 @@ class Api::V1::AccountController < Api::V1::ApiController
       ApplicationHelper.send_notification(title: I18n.t('pages.account.change_email.success'), user_id: @user.id)
     else
       render json: { error: I18n.t('api.errors.invalid_password') } #, status: 401 - force logout client 
+    end
+  end
+
+  def change_email_subscriptions
+    if @user.update(email_subscription_ids: params[:email_subscription_ids].to_a)
+      render json: { user: @user, notice: I18n.t('pages.account.change_email_subscription.success') }
+    else
+      render json: { user: @user, error: I18n.t('pages.account.change_email_subscription.error') }
     end
   end
 
