415cace8eafafdc87eda8fb391b59a7df0e52431
Dmitry K <kosenkodk@gmail.com>
2020-05-13 20:56:02 +0300

    fix seed data

--- app/controllers/api/v1/email_subscriptions_controller.rb
+++ app/controllers/api/v1/email_subscriptions_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::EmailSubscriptionsController < ApplicationController
   before_action :authorize_access_request!, except: :to_pdf
 
   def index
-    items = EmailSubscription.where(user: current_user).all
+    items = EmailSubscription.all
     render json: { email_subscriptions: items, email_subscription_ids: current_user.email_subscription_ids  }
   end
 end
--- app/models/email_subscription.rb
+++ app/models/email_subscription.rb
@@ -1,7 +1,4 @@
 class EmailSubscription < ApplicationRecord
-  # belongs_to :user, optional: true
-  # has_and_belongs_to_many :users
-
   has_many :users_email_subscriptions
   has_many :users, through: :users_email_subscriptions
 end
