dce4f1d8c814d99210e0dce5c157c3b169ac2b3e
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2020-05-14 12:44:19 +0300

    fix tests

--- spec/controllers/api/v1/email_subscriptions_controller_spec.rb
+++ spec/controllers/api/v1/email_subscriptions_controller_spec.rb
@@ -5,17 +5,16 @@ RSpec.describe Api::V1::EmailSubscriptionsController, type: :controller do
   before { sign_in_as(user) }
 
   describe 'GET #index' do
-    let!(:email_subscription) { create(:email_subscription, user: user) }
+    let!(:email_subscription) { create(:email_subscription, users: [user]) }
 
     it 'returns a success response' do
       get :index
       expect(response).to be_successful
-      expect(response_json.keys).to eq(['email_subscriptions', 'email_subscription_ids'])
+      expect(response_json.keys).to eq(['email_subscriptions', 'email_subscription_ids', 'user'])
       expect(response_json['email_subscriptions'].size).to eq 1
       expect(response_json['email_subscriptions'].first.values).to include(email_subscription.title)
       expect(response_json['email_subscriptions'].first.keys).to include('title')
       expect(response_json['email_subscriptions'].first.keys).to include('text')
-      expect(response_json['email_subscriptions'].first.keys).to include('user_id')
     end
 
     it 'unauth without cookie' do
