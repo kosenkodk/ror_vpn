91bf13c5e46eee67bb8c78ff37ec554f8e74ed8c
Dmitry K <kosenkodk@gmail.com>
2020-05-15 10:51:32 +0300

    send_newsletter_to_users

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,12 +1,14 @@
 class Email < ApplicationRecord
   after_create :send_newsletter_to_users
   
-  belongs_to :email_subscription#, optional: true
+  belongs_to :email_subscription, optional: true
 
   def send_newsletter_to_users
-    self.email_subscription.emails.where(is_published: true).each do |email|
-      self.email_subscription.users do |user|
-        UserMailer.newsletter(user, email, self.email_subscription)
+    email_subscription.emails.where(is_published: true).each do |email|
+      puts "email title: #{email.title} users count: #{email_subscription.users.count}"
+      email_subscription.users.each do |user|
+        puts "email send to user: #{user.email}"
+        UserMailer.newsletter(user, email, email_subscription)
       end
     end
   end
