9ea4b0d8cc90bcf4e7e3377aa6ddfc4401eff22f
Dmitry K <kosenkodk@gmail.com>
2020-05-29 20:49:13 +0300

    add user and admin to mongo db

--- vpn_server/mongo/createusers.sh
+++ vpn_server/mongo/createusers.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
-# set -e
 echo "Creating users..."
-# mongo admin --host localhost -u $MONGO_USERNAME -p $MONGO_PWD --eval "db.createUser({user: 'appuser', pwd: 'supersecret',roles: [{role: 'readWrite', db: 'MyAppDb'}]}); db.createUser({user: 'administrator', pwd: 'no1willguess', roles: [{role: 'userAdminAnyDatabase', db: 'admin'}]});"
 
-mongo admin --host $MONGO_HOST -u $MONGO_USERNAME -p $MONGO_PWD --eval "db.createUser({user: $MONGO_USERNAME, pwd: $MONGO_PWD , roles: [{role: 'userAdminAnyDatabase', db: 'admin'}]});"
-# mongo admin --host $MONGO_HOST -u $MONGO_USERNAME -p $MONGO_PWD --eval "db.createUser({user: $MONGO_USERNAME, pwd: $MONGO_PWD , roles: [{role: 'userAdminAnyDatabase', db: 'vpnUsers'}]});"
+mongo -- "$MONGO_INITDB_DATABASE" <<EOF
+  var rootUser = '$MONGO_INITDB_ROOT_USERNAME';
+  var rootPassword = '$MONGO_INITDB_ROOT_PASSWORD';
+  var admin = db.getSiblingDB('admin');
+  admin.auth(rootUser, rootPassword);
+
+  var user = '$MONGO_INITDB_USERNAME';
+  var passwd = '$MONGO_INITDB_PASSWORD';
+  db.createUser({user: user, pwd: passwd, roles: ["readWrite"]});
+EOF
+
 echo "Users created."
-# exec "$@"
